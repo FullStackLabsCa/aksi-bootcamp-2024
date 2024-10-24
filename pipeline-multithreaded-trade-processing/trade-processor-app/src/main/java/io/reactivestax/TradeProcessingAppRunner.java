75d9bf249e07759c3ff63fdc198d8f3caf1215b3
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-10-24 16:46:05 -0400

    Multi Thread Trade Processor - Version 4.1.2 - Processor - Removed Producer Files from service package and Main Runner

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/TradeProcessingAppRunner.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/TradeProcessingAppRunner.java
@@ -1,42 +1,13 @@
 package io.reactivestax;
 
-import io.reactivestax.service.ChunkProcessor;
 import io.reactivestax.service.TradeProcessor;
-import io.reactivestax.service.TradesFileReader;
-import io.reactivestax.utility.exceptions.InvalidAppModeException;
-
-import static io.reactivestax.utility.MultiThreadTradeProcessorUtility.*;
 
 public class TradeProcessingAppRunner {
 
     public static void main(String[] args) {
 
-        if(getFileProperty("trading.app.mode").equals("producer")) {
-            (new Thread(new FileReaderRunner())).start();
-            (new Thread(new ChunkProcessorRunner())).start();
-        } else if (getFileProperty("trading.app.mode").equals("consumer")) {
-            (new Thread(new TradeProcessorRunner())).start();
-        } else {
-            throw new InvalidAppModeException("App Mode Invalid.. Please Check the Properties File");
-        }
+        (new Thread(new TradeProcessorRunner())).start();
 
-    }
-}
-
-class FileReaderRunner implements Runnable{
-    @Override
-    public void run(){
-        String folderPath = getFileProperty("resourcesFolderPath");
-        TradesFileReader reader = new TradesFileReader();
-        reader.readFileAndCreateChunks(folderPath+"/"+getFileProperty("dataFileName"), null);
-    }
-}
-
-class ChunkProcessorRunner implements Runnable{
-    @Override
-    public void run(){
-        ChunkProcessor chunkProcessor = new ChunkProcessor();
-        chunkProcessor.startChunkProcessorPool();
     }
 }
 
--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/service/interfaces/TradesFileReading.java
+++
@@ -1,7 +0,0 @@
-package io.reactivestax.service.interfaces;
-
-public interface TradesFileReading {
-
-    void readFileAndCreateChunks(String filePath, String fileType);
-
-}
