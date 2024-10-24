1ba82bd06451a1abef919b7ec8660c421dbeeb24
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-10-24 16:02:22 -0400

    Multi Thread Trade Processor - Version 4.1.2 - Removed Processor Files from Bean Factory

--- pipeline-multithreaded-trade-processing/trade-producer-app/src/main/java/io/reactivestax/factory/BeanFactory.java
+++ pipeline-multithreaded-trade-processing/trade-producer-app/src/main/java/io/reactivestax/factory/BeanFactory.java
@@ -18,17 +18,11 @@ import io.reactivestax.utility.database.JDBCUtils;
 import io.reactivestax.utility.database.TransactionUtil;
 import io.reactivestax.utility.exceptions.InvalidMessagingTechnologyException;
 import io.reactivestax.utility.exceptions.InvalidPersistenceTechException;
-import io.reactivestax.utility.messaging.MessageReceiver;
-import io.reactivestax.utility.messaging.MessageRetry;
 import io.reactivestax.utility.messaging.MessageSender;
-import io.reactivestax.utility.messaging.inmemory.InMemoryReceiver;
-import io.reactivestax.utility.messaging.inmemory.InMemoryRetry;
 import io.reactivestax.utility.messaging.inmemory.InMemorySender;
-import io.reactivestax.utility.messaging.rabbitmq.RabbitMQReceiver;
-import io.reactivestax.utility.messaging.rabbitmq.RabbitMQRetry;
 import io.reactivestax.utility.messaging.rabbitmq.RabbitMQSender;
 
-import static io.reactivestax.utility.MultiThreadTradeProcessorUtility.getFileProperty;
+import static io.reactivestax.utility.ApplicationPropertyUtil.getFileProperty;
 
 public class BeanFactory {
 
@@ -40,19 +34,6 @@ public class BeanFactory {
     private static final String RABBIT_MQ_QUEUE_TECH = "rabbitmq";
     private static final String IN_MEMORY_QUEUE_TECH = "in-memory";
 
-    public static TransactionUtil getTransactionUtil(){
-        TransactionUtil transactionUtil;
-
-        if(getFileProperty("persistence.technology").equals(JDBC_PERSISTENCE_TECH)){
-            transactionUtil = JDBCUtils.getInstance();
-        } else if (getFileProperty("persistence.technology").equals(HIBERNATE_PERSISTENCE_TECH)){
-            transactionUtil = HibernateUtils.getInstance();
-        } else {
-            throw new InvalidPersistenceTechException();
-        }
-
-        return transactionUtil;
-    }
 
     public static RawPayloadRepo getRawPayloadRepo() {
         RawPayloadRepo rawPayloadRepo;
@@ -66,42 +47,6 @@ public class BeanFactory {
         }
 
         return rawPayloadRepo;
-    }
-
-    public static JournalEntryRepo getJournalEntryRepo() {
-        JournalEntryRepo journalEntryRepo;
-
-        if(getFileProperty("persistence.technology").equals(JDBC_PERSISTENCE_TECH)){
-            journalEntryRepo = JDBCJournalEntryRepo.getInstance();
-        } else if (getFileProperty("persistence.technology").equals(HIBERNATE_PERSISTENCE_TECH)){
-            journalEntryRepo = HibernateJournalEntryRepo.getInstance();
-        } else {
-            throw new InvalidPersistenceTechException();
-        }
-
-        return journalEntryRepo;
-    }
-
-    public static PositionsRepo getPositionsRepo() {
-        PositionsRepo positionsRepo;
-
-        if(getFileProperty("persistence.technology").equals(JDBC_PERSISTENCE_TECH)){
-            positionsRepo = JDBCPositionsRepo.getInstance();
-        } else if (getFileProperty("persistence.technology").equals(HIBERNATE_PERSISTENCE_TECH)){
-            positionsRepo = HibernatePositionsRepo.getInstance();
-        } else {
-            throw new InvalidPersistenceTechException();
-        }
-
-        return positionsRepo;
-    }
-
-    public static SecuritiesReferenceRepo getSecuritiesReferenceRepo() {
-        SecuritiesReferenceRepo securitiesReferenceRepo;
-
-        securitiesReferenceRepo = JDBCSecuritiesReferenceRepo.getInstance();
-
-        return securitiesReferenceRepo;
     }
 
     public static MessageSender<TradeIdAndAccNum> getMessageSender() {
@@ -118,31 +63,4 @@ public class BeanFactory {
         return messageSender;
     }
 
-    public static MessageReceiver<String> getMessageReceiver(){
-        MessageReceiver<String> messageReceiver;
-
-        if(getFileProperty("messaging.technology").equals(RABBIT_MQ_QUEUE_TECH)){
-            messageReceiver = RabbitMQReceiver.getInstance();
-        } else if (getFileProperty("messaging.technology").equals(IN_MEMORY_QUEUE_TECH)){
-            messageReceiver = InMemoryReceiver.getInstance();
-        } else {
-            throw new InvalidMessagingTechnologyException();
-        }
-
-        return messageReceiver;
-    }
-
-    public static MessageRetry<Trade> getMessageRetryer(){
-        MessageRetry<Trade> messageRetryer;
-
-        if(getFileProperty("messaging.technology").equals(RABBIT_MQ_QUEUE_TECH)){
-            messageRetryer = RabbitMQRetry.getInstance();
-        } else if (getFileProperty("messaging.technology").equals(IN_MEMORY_QUEUE_TECH)){
-            messageRetryer = InMemoryRetry.getInstance();
-        } else {
-            throw new InvalidMessagingTechnologyException();
-        }
-
-        return messageRetryer;
-    }
 }
