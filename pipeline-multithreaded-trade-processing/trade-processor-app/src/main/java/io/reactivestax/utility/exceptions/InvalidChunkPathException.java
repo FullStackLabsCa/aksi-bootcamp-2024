1a542ce3e6f216728335ce3ee194ab7b9963df71
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-10-25 09:03:35 -0400

    Multi Thread Trade Processor - Version 4.1.2 - Processor - Removed support for InMemory Messaging Tech

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/utility/exceptions/InvalidChunkPathException.java
+++
@@ -1,7 +0,0 @@
-package io.reactivestax.utility.exceptions;
-
-public class InvalidChunkPathException extends RuntimeException {
-    public InvalidChunkPathException(String s) {
-        System.out.println("Invalid Chunk Path Exception : "+s);
-    }
-}
