ba34085ca04f71fb1ad8b0498f9b472f15a8f157
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-04 13:18:14 -0500

    Multi Thread Trade Processor - Version 4.1.2 - Tests - ApplicationPropertyUtils - Done with 87% Line Coverage

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/utility/ApplicationPropertiesUtilsTest.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/utility/ApplicationPropertiesUtilsTest.java
@@ -1,6 +1,14 @@
 package io.reactivestax.utility;
 
+import org.junit.Test;
+
+import static io.reactivestax.utility.ApplicationPropertyUtils.getFileProperty;
+import static org.junit.Assert.assertEquals;
+
 public class ApplicationPropertiesUtilsTest {
 
-
+    @Test
+    public void testGetFileProperty(){
+        assertEquals("credit_card_transactions",getFileProperty("rabbitMQ.main.exchange.name"));
+    }
 }
