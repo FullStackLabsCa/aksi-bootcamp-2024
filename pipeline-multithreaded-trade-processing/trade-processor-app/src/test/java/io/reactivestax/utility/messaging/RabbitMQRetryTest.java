ef1e0fc1d76d5dae27849f87f6354124654f0cdc
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-06 13:24:51 -0500

    Multi Thread Trade Processor - Version 4.1.2 - Tests - RabbitMQRetryTest - tested getInstance

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/utility/messaging/RabbitMQRetryTest.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/utility/messaging/RabbitMQRetryTest.java
@@ -1,5 +1,44 @@
 package io.reactivestax.utility.messaging;
 
+import io.reactivestax.utility.messaging.rabbitmq.RabbitMQReceiver;
+import io.reactivestax.utility.messaging.rabbitmq.RabbitMQRetry;
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.assertEquals;
+
 public class RabbitMQRetryTest {
-    // Will test after Repo
+    @Test
+    public void getInstanceSingleThreadTest(){
+        // Get two instances
+        RabbitMQRetry instance1 = RabbitMQRetry.getInstance();
+        RabbitMQRetry instance2 = RabbitMQRetry.getInstance();
+
+        // Hashcode will be same
+        assertEquals(instance2.hashCode(), instance1.hashCode());
+
+        // Hashcode Identity will be same (reference to the same object)
+        assertEquals(System.identityHashCode(instance1), System.identityHashCode(instance2));
+    }
+
+    @Test
+    public void getInstanceMultiThreadTest() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        Callable<RabbitMQRetry> getInstance = RabbitMQRetry::getInstance;
+
+        // Get two instances
+        RabbitMQRetry instance1 = executorService.submit(getInstance).get();
+        RabbitMQRetry instance2 = executorService.submit(getInstance).get();
+
+        // Hashcode will be same
+        assertEquals(instance2.hashCode(), instance1.hashCode());
+
+        // Hashcode Identity will be same (reference to the same object)
+        assertEquals(System.identityHashCode(instance1), System.identityHashCode(instance2));
+    }
 }
