5d7ceffad8cfe214dd8753e96cfd221f920fb523
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-11 10:53:27 -0500

    Multi Thread Trade Processor - Version 4.8 - BeanFactory - Using Streams and Optional for Hibernate/JDBC Resource Providers

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/factory/BeanFactory.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/factory/BeanFactory.java
@@ -8,6 +8,7 @@ import io.reactivestax.repo.JournalEntryRepo;
 import io.reactivestax.repo.PositionsRepo;
 import io.reactivestax.repo.RawPayloadRepo;
 import io.reactivestax.repo.SecuritiesReferenceRepo;
+import io.reactivestax.repo.hibernate.HibernateSecuritiesReferenceRepo;
 import io.reactivestax.repo.jdbc.JDBCJournalEntryRepo;
 import io.reactivestax.repo.jdbc.JDBCPositionsRepo;
 import io.reactivestax.repo.jdbc.JDBCRawPayloadRepo;
@@ -26,6 +27,11 @@ import io.reactivestax.utility.messaging.rabbitmq.RabbitMQReceiver;
 import io.reactivestax.utility.messaging.rabbitmq.RabbitMQRetry;
 import io.reactivestax.utility.messaging.rabbitmq.RabbitMQUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+
 import static io.reactivestax.utility.ApplicationPropertyUtils.getFileProperty;
 
 public class BeanFactory {
@@ -33,73 +39,105 @@ public class BeanFactory {
     private BeanFactory() {
     }
 
-    private static final String JDBC_PERSISTENCE_TECH = "jdbc";
-    private static final String HIBERNATE_PERSISTENCE_TECH = "hibernate";
     private static final String RABBIT_MQ_QUEUE_TECH = "rabbitmq";
     private static final String IN_MEMORY_QUEUE_TECH = "in-memory";
+    private static final Map<String, Callable<Object>> jdbcTech = new HashMap<>();
+    private static final Map<String, Callable<Object>> hibernateTech = new HashMap<>();
+    private static final Map<String, Callable<Object>> rabbitMQTech = new HashMap<>();
+    private static final Map<String, Map<String, Callable<Object>>> mapOfTech = new HashMap<>();
+
+    static {
+        jdbcTech.put("transactionUtil", JDBCUtils::getInstance);
+        jdbcTech.put("rawPayloadRepo", JDBCRawPayloadRepo::getInstance);
+        jdbcTech.put("journalEntryRepo", JDBCJournalEntryRepo::getInstance);
+        jdbcTech.put("positionRepo", JDBCPositionsRepo::getInstance);
+        jdbcTech.put("securitiesReferenceRepo", JDBCSecuritiesReferenceRepo::getInstance);
+
+        hibernateTech.put("transactionUtil", HibernateUtils::getInstance);
+        hibernateTech.put("rawPayloadRepo", HibernateRawPayloadRepo::getInstance);
+        hibernateTech.put("journalEntryRepo", HibernateJournalEntryRepo::getInstance);
+        hibernateTech.put("positionRepo", HibernatePositionsRepo::getInstance);
+        hibernateTech.put("securitiesReferenceRepo", HibernateSecuritiesReferenceRepo::getInstance);
+
+        rabbitMQTech.put("messageReceiver", RabbitMQReceiver::getInstance);
+        rabbitMQTech.put("messageRetryer", RabbitMQRetry::getInstance);
+
+        mapOfTech.put("jdbc", jdbcTech);
+        mapOfTech.put("hibernate", hibernateTech);
+        mapOfTech.put("rabbitmq", rabbitMQTech);
+    }
+
+    private static Optional<Object> callSafely(Callable<Object> value) {
+        try{
+            return Optional.of(value.call());
+        } catch (Exception e){
+            return Optional.empty();
+        }
+    }
 
     public static TransactionUtil getTransactionUtil(){
-        TransactionUtil transactionUtil;
-
-        if(getFileProperty("persistence.technology").equals(JDBC_PERSISTENCE_TECH)){
-            transactionUtil = JDBCUtils.getInstance();
-        } else if (getFileProperty("persistence.technology").equals(HIBERNATE_PERSISTENCE_TECH)){
-            transactionUtil = HibernateUtils.getInstance();
-        } else {
-            throw new InvalidPersistenceTechException();
-        }
-
-        return transactionUtil;
+        return mapOfTech.entrySet().stream()
+                .filter(techMap -> techMap.getKey().equals(getFileProperty("persistence.technology")))
+                .map(Map.Entry::getValue)
+                .flatMap(techMapValues -> techMapValues.entrySet().stream())
+                .filter(resource -> resource.getKey().equals("transactionUtil"))
+                .map(resource -> callSafely(resource.getValue()))
+                .flatMap(Optional::stream)
+                .map(TransactionUtil.class::cast)
+                .findFirst()
+                .orElseThrow(InvalidPersistenceTechException::new);
     }
 
     public static RawPayloadRepo getRawPayloadRepo() {
-        RawPayloadRepo rawPayloadRepo;
-
-        if(getFileProperty("persistence.technology").equals(JDBC_PERSISTENCE_TECH)){
-            rawPayloadRepo = JDBCRawPayloadRepo.getInstance();
-        } else if (getFileProperty("persistence.technology").equals(HIBERNATE_PERSISTENCE_TECH)){
-            rawPayloadRepo = HibernateRawPayloadRepo.getInstance();
-        } else {
-            throw new InvalidPersistenceTechException();
-        }
-
-        return rawPayloadRepo;
+        return mapOfTech.entrySet().stream()
+                .filter(techMap -> techMap.getKey().equals(getFileProperty("persistence.technology")))
+                .map(Map.Entry::getValue)
+                .flatMap(techMapValues -> techMapValues.entrySet().stream())
+                .filter(resource -> resource.getKey().equals("rawPayloadRepo"))
+                .map(resource -> callSafely(resource.getValue()))
+                .flatMap(Optional::stream)
+                .map(RawPayloadRepo.class::cast)
+                .findFirst()
+                .orElseThrow(InvalidPersistenceTechException::new);
     }
 
     public static JournalEntryRepo getJournalEntryRepo() {
-        JournalEntryRepo journalEntryRepo;
-
-        if(getFileProperty("persistence.technology").equals(JDBC_PERSISTENCE_TECH)){
-            journalEntryRepo = JDBCJournalEntryRepo.getInstance();
-        } else if (getFileProperty("persistence.technology").equals(HIBERNATE_PERSISTENCE_TECH)){
-            journalEntryRepo = HibernateJournalEntryRepo.getInstance();
-        } else {
-            throw new InvalidPersistenceTechException();
-        }
-
-        return journalEntryRepo;
+        return mapOfTech.entrySet().stream()
+                .filter(techMap -> techMap.getKey().equals(getFileProperty("persistence.technology")))
+                .map(Map.Entry::getValue)
+                .flatMap(techMapValues -> techMapValues.entrySet().stream())
+                .filter(resource -> resource.getKey().equals("journalEntryRepo"))
+                .map(resource -> callSafely(resource.getValue()))
+                .flatMap(Optional::stream)
+                .map(JournalEntryRepo.class::cast)
+                .findFirst()
+                .orElseThrow(InvalidPersistenceTechException::new);
     }
 
     public static PositionsRepo getPositionsRepo() {
-        PositionsRepo positionsRepo;
-
-        if(getFileProperty("persistence.technology").equals(JDBC_PERSISTENCE_TECH)){
-            positionsRepo = JDBCPositionsRepo.getInstance();
-        } else if (getFileProperty("persistence.technology").equals(HIBERNATE_PERSISTENCE_TECH)){
-            positionsRepo = HibernatePositionsRepo.getInstance();
-        } else {
-            throw new InvalidPersistenceTechException();
-        }
-
-        return positionsRepo;
+        return mapOfTech.entrySet().stream()
+                .filter(techMap -> techMap.getKey().equals(getFileProperty("persistence.technology")))
+                .map(Map.Entry::getValue)
+                .flatMap(techMapValues -> techMapValues.entrySet().stream())
+                .filter(resource -> resource.getKey().equals("positionRepo"))
+                .map(resource -> callSafely(resource.getValue()))
+                .flatMap(Optional::stream)
+                .map(PositionsRepo.class::cast)
+                .findFirst()
+                .orElseThrow(InvalidPersistenceTechException::new);
     }
 
     public static SecuritiesReferenceRepo getSecuritiesReferenceRepo() {
-        SecuritiesReferenceRepo securitiesReferenceRepo;
-
-        securitiesReferenceRepo = JDBCSecuritiesReferenceRepo.getInstance();
-
-        return securitiesReferenceRepo;
+        return mapOfTech.entrySet().stream()
+                .filter(techMap -> techMap.getKey().equals(getFileProperty("persistence.technology")))
+                .map(Map.Entry::getValue)
+                .flatMap(techMapValues -> techMapValues.entrySet().stream())
+                .filter(resource -> resource.getKey().equals("securitiesReferenceRepo"))
+                .map(resource -> callSafely(resource.getValue()))
+                .flatMap(Optional::stream)
+                .map(SecuritiesReferenceRepo.class::cast)
+                .findFirst()
+                .orElseThrow(InvalidPersistenceTechException::new);
     }
 
     public static MessageReceiver<String> getMessageReceiver(){
