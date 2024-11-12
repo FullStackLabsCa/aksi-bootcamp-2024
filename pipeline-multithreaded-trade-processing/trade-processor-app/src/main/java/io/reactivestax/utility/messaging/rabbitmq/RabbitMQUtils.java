ac9275d7c1f2e9f3e8ba2046b268ce6210c71281
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-12 17:18:07 -0500

    Multi Thread Trade Processor - Version 4.8 - Testing - RabbitMQRetryTest - closeRabbitMQConnection is resetting the factory and connection.

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/utility/messaging/rabbitmq/RabbitMQUtils.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/utility/messaging/rabbitmq/RabbitMQUtils.java
@@ -49,7 +49,13 @@ public class RabbitMQUtils {
     }
 
     public void closeRabbitMQConnection() throws IOException {
-        rabbitMQConnection.close();
+        try {
+            rabbitMQConnection.close();
+        } catch (IOException | NullPointerException e) {
+            System.out.println("No Connection Exists");
+        }
+        rabbitMQFactory = null;
+        rabbitMQConnection = null;
     }
 
     public Channel getRabbitMQChannel() {
