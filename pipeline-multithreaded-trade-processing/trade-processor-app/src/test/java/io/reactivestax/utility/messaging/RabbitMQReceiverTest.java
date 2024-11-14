1227dea16e0ff9e87601a336f876a87adf8f71a1
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-14 13:50:45 -0500

    Multi Thread Trade Processor - Version 4.8 - Testing - RabbitMQReceiverTest - Covered Additional Exceptional Case

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/utility/messaging/RabbitMQReceiverTest.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/utility/messaging/RabbitMQReceiverTest.java
@@ -6,10 +6,15 @@ import io.reactivestax.utility.ApplicationPropertyUtils;
 import io.reactivestax.utility.messaging.rabbitmq.RabbitMQReceiver;
 import io.reactivestax.utility.messaging.rabbitmq.RabbitMQUtils;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.concurrent.Callable;
@@ -18,10 +23,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static io.reactivestax.utility.ApplicationPropertyUtils.getFileProperty;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class RabbitMQReceiverTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Spy
+    private RabbitMQReceiver rabbitMQReceiver = RabbitMQReceiver.getInstance();
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
 
     @After
     public void cleanUp(){
@@ -107,5 +121,25 @@ public class RabbitMQReceiverTest {
                 messageProvider = BeanFactory.getMessageProvider(0);
                 assertTrue(messageReceiver.receiveMessage(messageProvider).isEmpty());
             }
+    }
+
+    @Test
+    public void rabbitMQThrowExceptionTest(){
+        System.setOut(new PrintStream(outputStreamCaptor));
+        try (MockedStatic<ApplicationPropertyUtils> mockedStatic = Mockito.mockStatic(ApplicationPropertyUtils.class)) {
+            mockedStatic.when(() -> getFileProperty("messaging.technology")).thenReturn("rabbitmq");
+            mockedStatic.when(() -> getFileProperty("rabbitMQ.main.exchange.name")).thenReturn("credit_card_transactions");
+            mockedStatic.when(() -> getFileProperty("rabbitMQ.main.queue0.name")).thenReturn("cc_partition_0_queue");
+            mockedStatic.when(() -> getFileProperty("rabbitMQ.main.queue0.routingKey")).thenReturn("cc_partition_0");
+            mockedStatic.when(() -> getFileProperty("rabbitMQ.retry.exchange.name")).thenReturn("retry_exchange");
+            mockedStatic.when(() -> getFileProperty("rabbitMQ.hostName")).thenReturn("localhost");
+            mockedStatic.when(() -> getFileProperty("rabbitMQ.guest")).thenReturn("guest");
+            mockedStatic.when(() -> getFileProperty("rabbitMQ.pass")).thenReturn("guest");
+
+            MessageReceiver<String> messageReceiver = rabbitMQReceiver;
+            messageReceiver.receiveMessage(null);
+            assertTrue(outputStreamCaptor.toString().contains("Error Initializing RabbitMQ Receiver Main...."));
+        }
+        System.setOut(originalOut);
     }
 }
