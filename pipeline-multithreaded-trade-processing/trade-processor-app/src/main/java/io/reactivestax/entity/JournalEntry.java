53fc51169c59f090330af994df7dc08c5bdcc5ce
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-15 09:54:21 -0500

    Multi Thread Trade Processor - Version 4.8 - Testing - HibernateJournalEntryRepo - Added equals and hashcode implementation for JournalEntry

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/entity/JournalEntry.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/entity/JournalEntry.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.sql.Date;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -41,4 +42,17 @@ public class JournalEntry {
 
     @Column (name = "trade_id")
     private String tradeID;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JournalEntry that = (JournalEntry) o;
+        return securityID == that.securityID && quantity == that.quantity && Objects.equals(accountNumber, that.accountNumber) && Objects.equals(activity, that.activity) && Objects.equals(tradeExecutionTime, that.tradeExecutionTime) && Objects.equals(positionPostedStatus, that.positionPostedStatus) && Objects.equals(tradeID, that.tradeID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, securityID, activity, quantity, tradeExecutionTime, positionPostedStatus, tradeID);
+    }
 }
