e681692df92bcd69aff846384c344326392acdf2
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-15 12:19:34 -0500

    Multi Thread Trade Processor - Version 4.8 - Testing - Added More Suppliers for Trade

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/TestDataProvider.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/TestDataProvider.java
@@ -26,4 +26,34 @@ public interface TestDataProvider {
             .cusip("TSLA")
             .quantity(10)
             .build();
+
+    Supplier<Trade> goodBuyTradeSupplier = () -> Trade.builder()
+            .tradeID("TD123")
+            .accountNumber("123")
+            .activity("BUY")
+            .price(0.0)
+            .transactionTime(new java.sql.Date(2024))
+            .cusip("TSLA")
+            .quantity(10)
+            .build();
+
+    Supplier<Trade> goodSellTradeSupplier = () -> Trade.builder()
+            .tradeID("TD123")
+            .accountNumber("123")
+            .activity("SELL")
+            .price(0.0)
+            .transactionTime(new java.sql.Date(2024))
+            .cusip("TSLA")
+            .quantity(10)
+            .build();
+
+    Supplier<Trade> invalidActivityTradeSupplier = () -> Trade.builder()
+            .tradeID("TD123")
+            .accountNumber("123")
+            .activity("Invalid")
+            .price(0.0)
+            .transactionTime(new java.sql.Date(2024))
+            .cusip("TSLA")
+            .quantity(10)
+            .build();
 }
