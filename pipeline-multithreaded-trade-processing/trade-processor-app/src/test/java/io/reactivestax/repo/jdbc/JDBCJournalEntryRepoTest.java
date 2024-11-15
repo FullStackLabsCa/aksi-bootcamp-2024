d7b68edafc7045199887734945b5446313ea8a9c
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-15 16:01:58 -0500

    Multi Thread Trade Processor - Version 4.8 - Testing - JDBCJournalEntryRepo (Written Against Real DB) - Tested GetInstance

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/repo/jdbc/JDBCJournalEntryRepoTest.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/repo/jdbc/JDBCJournalEntryRepoTest.java
@@ -1,4 +1,43 @@
 package io.reactivestax.repo.jdbc;
 
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.assertEquals;
+
 public class JDBCJournalEntryRepoTest {
+
+    @Test
+    public void getInstanceSingleThreadTest() {
+        // Get two instances
+        JDBCJournalEntryRepo instance1 = JDBCJournalEntryRepo.getInstance();
+        JDBCJournalEntryRepo instance2 = JDBCJournalEntryRepo.getInstance();
+
+        // Hashcode will be same
+        assertEquals(instance2.hashCode(), instance1.hashCode());
+
+        // Hashcode Identity will be same (reference to the same object)
+        assertEquals(System.identityHashCode(instance1), System.identityHashCode(instance2));
+    }
+
+    @Test
+    public void getInstanceMultiThreadTest() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        Callable<JDBCJournalEntryRepo> getInstance = JDBCJournalEntryRepo::getInstance;
+
+        // Get two instances
+        JDBCJournalEntryRepo instance1 = executorService.submit(getInstance).get();
+        JDBCJournalEntryRepo instance2 = executorService.submit(getInstance).get();
+
+        // Hashcode will be same
+        assertEquals(instance2.hashCode(), instance1.hashCode());
+
+        // Hashcode Identity will be same (reference to the same object)
+        assertEquals(System.identityHashCode(instance1), System.identityHashCode(instance2));
+    }
 }
