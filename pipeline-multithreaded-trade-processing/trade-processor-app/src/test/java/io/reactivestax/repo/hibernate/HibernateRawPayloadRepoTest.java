d726ea3f92abe79a7780ef520a1fc803529ca3dc
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-17 18:01:52 -0500

    Multi Thread Trade Processor - Version 4.8 - Testing - Tested readPayloadFromRawPayloadTable

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/repo/hibernate/HibernateRawPayloadRepoTest.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/repo/hibernate/HibernateRawPayloadRepoTest.java
@@ -1,7 +1,13 @@
 package io.reactivestax.repo.hibernate;
 
+import io.reactivestax.TestDataProvider;
+import io.reactivestax.entity.RawPayload;
+import io.reactivestax.utility.database.HibernateUtils;
+import org.hibernate.Session;
+import org.junit.After;
 import org.junit.Test;
 
+import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -10,6 +16,20 @@ import java.util.concurrent.Executors;
 import static org.junit.Assert.assertEquals;
 
 public class HibernateRawPayloadRepoTest {
+
+    @After
+    public void cleanUp(){
+        try {
+            HibernateUtils.getInstance().startTransaction();
+            String sql = "delete from RawPayload";
+            jakarta.persistence.Query query = HibernateUtils.getInstance().getConnection().createQuery(sql);
+            query.executeUpdate();
+            HibernateUtils.getInstance().commitTransaction();
+        } catch (Exception e) {
+            e.printStackTrace();
+            HibernateUtils.getInstance().rollbackTransaction();
+        }
+    }
 
     @Test
     public void getInstanceSingleThreadTest() {
@@ -40,4 +60,55 @@ public class HibernateRawPayloadRepoTest {
         // Hashcode Identity will be same (reference to the same object)
         assertEquals(System.identityHashCode(instance1), System.identityHashCode(instance2));
     }
+
+    @Test
+    public void readPayloadFromRawPayload_tradeIdExists_Test(){
+        String tradePayload = TestDataProvider.validTradePayloadSupplier.get();
+
+        // Insert the Trade with Payload into the DB
+        Session session = HibernateUtils.getInstance().getConnection();
+        HibernateUtils.getInstance().startTransaction();
+        try {
+            RawPayload rawPayload = RawPayload.builder()
+                    .tradeID("TDB_00000001")
+                    .status("Valid")
+                    .payload(tradePayload)
+                    .lookupStatus("Non Posted")
+                    .postedStatus("Non Posted")
+                    .build();
+
+            session.persist(rawPayload);
+
+            HibernateUtils.getInstance().commitTransaction();
+        } catch (Exception e) {
+            HibernateUtils.getInstance().rollbackTransaction();
+        }
+
+        // Read the payload for the same trade ID, it will be the same as that of Inserted
+        Optional<String> payloadReadFromRawPayloadTable = HibernateRawPayloadRepo.getInstance().readPayloadFromRawPayloadsTable("TDB_00000001");
+        assertEquals(tradePayload, payloadReadFromRawPayloadTable.get());
+    }
+
+    @Test
+    public void readPayloadFromRawPayload_TradeIdDoesNotExists_Test(){
+        // Read the Payload for a tradeId, It will be Optional.empty()
+        Optional<String> payloadReadFromRawPayloadTable = HibernateRawPayloadRepo.getInstance().readPayloadFromRawPayloadsTable("test-trade");
+        assertEquals(Optional.empty(), payloadReadFromRawPayloadTable);
+    }
+
+    /*
+    Update Security LookUp Status in RawPayloads Table
+    -   Check Before
+    -   Update ( Valid # TODO 3 and Invalid # TODO 4)
+    -   Check After - Should Match the Updated
+    - Exception Occured # TODO 5
+     */
+
+    /*
+    Update Journal Entry Status in Raw Payload Table
+    -   Check Before
+    -   Update Posted
+    -   Check After Should be posted # TODO 6
+    - Exception Occured # TODO 7
+     */
 }
