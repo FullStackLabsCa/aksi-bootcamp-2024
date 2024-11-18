52c81903902eb608cdd8112a11aa57ae5e462714
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-17 19:32:50 -0500

    Multi Thread Trade Processor - Version 4.8 - Testing - JDBCRawPayloadRepo (Against Read DB) - Added Tests for Update Security LookUPStatus and Position Posted Status

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/repo/jdbc/JDBCRawPayloadRepoTest.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/repo/jdbc/JDBCRawPayloadRepoTest.java
@@ -1,29 +1,37 @@
 package io.reactivestax.repo.jdbc;
 
 import io.reactivestax.TestDataProvider;
+import io.reactivestax.model.Trade;
 import io.reactivestax.utility.database.JDBCUtils;
+import io.reactivestax.utility.exceptions.UpdateJournalEntryStatusInRawPayloadFailed;
 import org.junit.After;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class JDBCRawPayloadRepoTest {
 
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
-    private static final String INSERT_INTO_TRADES_PAYLOAD_QUERY = "Insert into trades_payload (trade_id, status, payload, postedStatus) values (?,?,?, 'Not Posted')";
+    private static final String INSERT_INTO_TRADES_PAYLOAD_QUERY = "Insert into trades_payload (trade_id, status, payload, lookupStatus, postedStatus) values (?,?,?, 'Not Posted', 'Not Posted')";
+    private static final String READ_SECURITYLOOKUPSTATUS_QUERY = "Select lookupStatus from trades_payload where trade_id=?";
+    private static final String READ_POSITIONUPDATESTATUS_QUERY = "Select postedStatus from trades_payload where trade_id=?";
 
     @After
     public void cleanUp(){
@@ -106,5 +114,79 @@ public class JDBCRawPayloadRepoTest {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideLookUpStatusForTest")
+    void updateSecurityLookUpStatusTest(String lookupStatus) throws SQLException {
+        Trade trade = TestDataProvider.validTradeForPayloadSupplier.get();
+        insertIntoRawPayloadTable("TDB_00000001", TestDataProvider.validTradePayloadSupplier.get());
+
+        JDBCRawPayloadRepo.getInstance().updateSecurityLookupStatusInRawPayloadsTable(trade, lookupStatus);
+        String securityLookUpStatusAfterUpdate = readSecurityLookUpStatusFromRawPayload("TDB_00000001");
+
+        if (lookupStatus.equals("Valid"))
+            assertEquals("Succeeded", securityLookUpStatusAfterUpdate);
+        else
+            assertEquals("Failed", securityLookUpStatusAfterUpdate);
+    }
+
+    private String readSecurityLookUpStatusFromRawPayload(String tradeID) throws SQLException {
+        Connection connection = JDBCUtils.getInstance().getConnection();
+        try (PreparedStatement psQuery = connection.prepareStatement(READ_SECURITYLOOKUPSTATUS_QUERY)) {
+
+            psQuery.setString(1, tradeID);
+            ResultSet rsQuery = psQuery.executeQuery();
+            rsQuery.next();
+            return rsQuery.getString("lookupStatus");
+        }
+    }
+
+    static Stream<Arguments> provideLookUpStatusForTest() {
+        return Stream.of(
+                Arguments.of("Valid"),
+                Arguments.of("Invalid")
+        );
+    }
+
+    @Test
+    public void updateSecurityLookUpStatusFailedTest(){
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        JDBCRawPayloadRepo.getInstance().updateSecurityLookupStatusInRawPayloadsTable(null, null);
+        assertTrue(outputStreamCaptor.toString().contains("Failed to Update Security Lookup Status in Raw-Payload Table"));
+
+        System.setOut(originalOut);
+    }
+
+    private String readPostedStatusFromRawPayload(String tradeID) throws SQLException {
+        Connection connection = JDBCUtils.getInstance().getConnection();
+        try (PreparedStatement psQuery = connection.prepareStatement(READ_POSITIONUPDATESTATUS_QUERY)) {
+
+            psQuery.setString(1, tradeID);
+            ResultSet rsQuery = psQuery.executeQuery();
+            rsQuery.next();
+            return rsQuery.getString("postedStatus");
+        }
+    }
+
+    @Test
+    public void updateJournalEntryStatusTest() throws UpdateJournalEntryStatusInRawPayloadFailed, SQLException {
+        Trade trade = TestDataProvider.validTradeForPayloadSupplier.get();
+        insertIntoRawPayloadTable("TDB_00000001", TestDataProvider.validTradePayloadSupplier.get());
+
+        String postedStatusBeforeUpdate = readPostedStatusFromRawPayload("TDB_00000001");
+        assertEquals("Not Posted", postedStatusBeforeUpdate);
+
+        JDBCRawPayloadRepo.getInstance().updateJournalEntryStatusInRawPayloadsTable(trade);
+
+        String postedStatusAfterUpdate = readPostedStatusFromRawPayload("TDB_00000001");
+
+        assertEquals("Posted", postedStatusAfterUpdate);
+    }
+
+    @Test
+    public void updateJournalEntryStatusFailedTest() {
+        assertThrows(UpdateJournalEntryStatusInRawPayloadFailed.class, () -> JDBCRawPayloadRepo.getInstance().updateJournalEntryStatusInRawPayloadsTable(null));
     }
 }
