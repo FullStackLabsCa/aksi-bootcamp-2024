571166367744fcf81b3fd959b0cbe3c44b1dbfc6
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-18 12:24:16 -0500

    Multi Thread Trade Processor - Version 4.8 - Producer Codebase - BeanFactoryTest - Fixing use of junit4 instead of 5

--- pipeline-multithreaded-trade-processing/trade-producer-app/src/test/java/io/reactivestax/factory/BeanFactoryTest.java
+++ pipeline-multithreaded-trade-processing/trade-producer-app/src/test/java/io/reactivestax/factory/BeanFactoryTest.java
@@ -3,16 +3,22 @@ package io.reactivestax.factory;
 import io.reactivestax.repo.RawPayloadRepo;
 import io.reactivestax.repo.hibernate.HibernateRawPayloadRepo;
 import io.reactivestax.repo.jdbc.JDBCRawPayloadRepo;
+import io.reactivestax.service.interfaces.TradeIdAndAccNum;
 import io.reactivestax.utility.ApplicationPropertyUtils;
 import io.reactivestax.utility.database.HibernateUtils;
 import io.reactivestax.utility.database.JDBCUtils;
+import io.reactivestax.utility.exceptions.InvalidMessagingTechnologyException;
 import io.reactivestax.utility.exceptions.InvalidPersistenceTechException;
+import io.reactivestax.utility.messaging.MessageSender;
+import io.reactivestax.utility.messaging.inmemory.InMemorySender;
+import io.reactivestax.utility.messaging.rabbitmq.RabbitMQSender;
 import org.junit.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import static io.reactivestax.utility.ApplicationPropertyUtils.getFileProperty;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class BeanFactoryTest {
 
@@ -64,5 +70,48 @@ public class BeanFactoryTest {
 
             assertThrows(InvalidPersistenceTechException.class, BeanFactory::getRawPayloadRepo);
         }
+    }
+
+    private void withMockedProperty(String propertyName, String value, Runnable functionToExecute){
+        try(MockedStatic<ApplicationPropertyUtils> mockedStatic = Mockito.mockStatic(ApplicationPropertyUtils.class)) {
+            mockedStatic.when(() -> getFileProperty(propertyName)).thenReturn(value);
+            functionToExecute.run();
+        }
+    }
+
+    @Test
+    public void testGetMessageReceiver_RabbitMQ(){
+        Runnable test = () -> {
+            MessageSender<TradeIdAndAccNum> messageReceiver = BeanFactory.getMessageSender();
+            MessageSender<TradeIdAndAccNum> rabbitMQReceiver = RabbitMQSender.getInstance();
+
+            assertNotNull(messageReceiver);
+            assertInstanceOf(RabbitMQSender.class, messageReceiver);
+            // Singleton Verification
+            assertEquals(messageReceiver, rabbitMQReceiver);
+        };
+
+        withMockedProperty("messaging.technology","rabbitmq", test);
+    }
+
+    @Test
+    public void testGetMessageReceiver_InMemory(){
+        Runnable test = () -> {
+            MessageSender<TradeIdAndAccNum> messageSender = BeanFactory.getMessageSender();
+            MessageSender<TradeIdAndAccNum> inMemorySender = InMemorySender.getInstance();
+
+            assertNotNull(messageSender);
+            assertInstanceOf(InMemorySender.class, messageSender);
+            // Singleton Verification
+            assertEquals(messageSender, inMemorySender);
+        };
+        withMockedProperty("messaging.technology","in-memory", test);
+    }
+
+    @Test
+    public void testGetMessageReceiver_InvalidTech(){
+        Runnable test = () -> assertThrows(InvalidMessagingTechnologyException.class, BeanFactory::getMessageSender);
+
+        withMockedProperty("messaging.technology","invalid",test);
     }
 }
