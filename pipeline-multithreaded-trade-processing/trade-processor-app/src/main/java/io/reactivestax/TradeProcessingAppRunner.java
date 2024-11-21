6b0df2ffc6ca6974e65eb4d4f11ba485b896fb33
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-21 14:11:49 -0500

    Multi Thread Trade Processor - Version 4.8 - Consumer Codebase - TradeProcessingAppRunner - Removed The Runner Class and replaced with the use of Lambda

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/TradeProcessingAppRunner.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/TradeProcessingAppRunner.java
@@ -5,16 +5,7 @@ import io.reactivestax.service.TradeProcessor;
 public class TradeProcessingAppRunner {
 
     public static void main(String[] args) {
-
-        (new Thread(new TradeProcessorRunner())).start();
-
-    }
-}
-
-class TradeProcessorRunner implements Runnable{
-    @Override
-    public void run(){
-        TradeProcessor processor = new TradeProcessor();
-        processor.startTradesProcessing();
+        TradeProcessor tradeProcessor = new TradeProcessor();
+        (new Thread(tradeProcessor::startTradesProcessing)).start();
     }
 }
