e637358d65094a8662b5ce73a9eecb91dd36e106
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-21 14:20:15 -0500

    Multi Thread Trade Processor - Version 4.8 - Consumer Codebase - Testing - TradeProcessorService - getInstance tested

---
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/test/java/io/reactivestax/service/TradeProcessorServiceTest.java
@@ -0,0 +1,46 @@
+package io.reactivestax.service;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TradeProcessorServiceTest {
+
+    @Test
+    void getInstanceSingleThreadTest(){
+        // Get two instances
+        TradeProcessorService instance1 = TradeProcessorService.getInstance();
+        TradeProcessorService instance2 = TradeProcessorService.getInstance();
+
+        // Hashcode will be same
+        assertEquals(instance2.hashCode(), instance1.hashCode());
+
+        // Hashcode Identity will be same (reference to the same object)
+        assertEquals(System.identityHashCode(instance1), System.identityHashCode(instance2));
+    }
+
+    @Test
+    void getInstanceMultiThreadTest() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        Callable<TradeProcessorService> getInstance = TradeProcessorService::getInstance;
+
+        // Get two instances
+        TradeProcessorService instance1 = executorService.submit(getInstance).get();
+        TradeProcessorService instance2 = executorService.submit(getInstance).get();
+
+        // Hashcode will be same
+        assertEquals(instance2.hashCode(), instance1.hashCode());
+
+        // Hashcode Identity will be same (reference to the same object)
+        assertEquals(System.identityHashCode(instance1), System.identityHashCode(instance2));
+    }
+
+}
