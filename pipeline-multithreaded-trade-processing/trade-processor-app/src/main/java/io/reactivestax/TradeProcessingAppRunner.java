40ea1ba8299be3ea9003e7ea65cbd5555c0279b7
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-11-25 10:33:33 -0500

    Multi Thread Trade Processor - Version 4.8 - Consumer Codebase - Fat Jar Creation and added shut down hook

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/TradeProcessingAppRunner.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/TradeProcessingAppRunner.java
@@ -7,5 +7,7 @@ public class TradeProcessingAppRunner {
     public static void main(String[] args) {
         TradeProcessor tradeProcessor = new TradeProcessor();
         (new Thread(tradeProcessor::startTradesProcessing)).start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("Shutdown hook triggered. Cleaning up...")));
     }
 }
