b2a7c7eed4d4b3ae5f6eeadd8fcf66bfa76026df
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-12-04 13:56:43 -0500

    Multi Thread Trade Processor - Version 4.8 - Producer Codebase - Testing Covered Exception Case for HibernateRawPayloadRepoTest - writeToRawPayloadTable

--- pipeline-multithreaded-trade-processing/trade-producer-app/src/test/java/io/reactivestax/repo/HibernateRawPayloadRepoTest.java
+++ pipeline-multithreaded-trade-processing/trade-producer-app/src/test/java/io/reactivestax/repo/HibernateRawPayloadRepoTest.java
@@ -5,11 +5,16 @@ import io.reactivestax.entity.RawPayload;
 import io.reactivestax.repo.hibernate.HibernateRawPayloadRepo;
 import io.reactivestax.utility.ApplicationPropertyUtils;
 import io.reactivestax.utility.database.HibernateUtils;
+import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.sql.Connection;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -17,12 +22,20 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
-
+@ExtendWith(MockitoExtension.class)
 public class HibernateRawPayloadRepoTest {
+
+    @Spy
+    private HibernateUtils hibernateUtilsSpy;
+
+    @InjectMocks
+    private HibernateRawPayloadRepo hibernateRawPayloadRepo;
 
     @BeforeEach
     void setUp(){
+        MockitoAnnotations.openMocks(this);
         ApplicationPropertyUtils.readPropertiesFile("src/test/resources/test.application.properties");
     }
 
@@ -101,6 +114,21 @@ public class HibernateRawPayloadRepoTest {
         return query.getResultList();
     }
 
-//    @Test # TODO
-//    writeToRawPayloadExceptionTest
+    @Test
+    void writeToRawPayloadExceptionTest(){
+        Session session = HibernateUtils.getInstance().getConnection();
+        try(MockedStatic<HibernateUtils> hibernateUtilsMockedStatic = Mockito.mockStatic(HibernateUtils.class)){
+            //Setup
+            hibernateUtilsMockedStatic.when(HibernateUtils::getInstance).thenReturn(hibernateUtilsSpy);
+            doReturn(session).when(hibernateUtilsSpy).getConnection();
+            doAnswer(invocationOnMock -> {throw new Exception();}).when(hibernateUtilsSpy).startTransaction();
+
+            //Action
+            hibernateRawPayloadRepo.writeToRawPayloadTable(TestDataProvider.validRawPayloadSupplier.get());
+
+            //Assertion
+            verify(hibernateUtilsSpy, times(0)).commitTransaction();
+            verify(hibernateUtilsSpy, times(1)).rollbackTransaction();
+        }
+    }
 }
