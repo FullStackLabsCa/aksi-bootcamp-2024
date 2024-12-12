9be118fb13fefd16dd77cf4cfb7706d7a34fbb5e
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-12-12 16:51:43 -0500

    BNYM Parser - Added the Node class Maintaining the leftID, rightID, key, value

---
+++ bnym-parser/src/main/java/org/example/Node.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Node {
+    private String type;
+    private int leftID;
+    private int rightID;
+    private String value;
+    private String rulesetID;
+    private String account;
+}
