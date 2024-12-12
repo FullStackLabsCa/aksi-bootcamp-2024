7e210bce567ee85cd16ce9a31eeb39e6b16ce8dc
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-12-12 17:22:20 -0500

    BNYM Parser - Working on the Implementation of the Algorithm for Adding Data to the tree and assigning left and right ID's

--- bnym-parser/src/main/java/org/example/FileReader.java
+++ bnym-parser/src/main/java/org/example/FileReader.java
@@ -1,20 +1,82 @@
 package org.example;
 
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class FileReader {
 
+    HashMap<String, List<Node>> generalTree = new HashMap<>();
+    int indexCounter = 0;
+    Stack<Node> stack = new Stack<>();
+
+
     public void readFile(String filePath) {
+        String rulesetID = "";
+
         try (Scanner fileReader = new Scanner(Objects.requireNonNull(FileReader.class.getClassLoader().getResourceAsStream(filePath)))) {
 
             while (fileReader.hasNextLine()) {
-                String line = fileReader.nextLine();
+                String line = fileReader.nextLine().trim();
                 String key = line.substring(0,2);
                 String value = line.substring(2);
-                System.out.println("key = " + key);
-                System.out.println("value = " + value);
+
+                if(!generalTree.containsKey(key)){
+                    generalTree.put(key, new ArrayList<>());
+                }
+
+                if(key.equals("01")){
+                    publishMapToDB();
+                    clearStack();
+                    rulesetID = value;
+                } else if (Integer.parseInt(key) <= Integer.parseInt(stack.peek().getType())){
+                    updateNodeInStack();
+                }
+
+                Node node = createNode(key, value, rulesetID);
+
+                if(key.equals("01") || key.equals("03") || key.equals("04")) stack.push(node);
+                else generalTree.get(key).add(node);
+
             }
+        }
+    }
+
+    private void updateNodeInStack() {
+        Node poppedNode = stack.pop();
+        poppedNode.setRightID(indexCounter + 1);
+        indexCounter++;
+        generalTree.get(poppedNode.getType()).add(poppedNode);
+    }
+
+    private void clearStack() {
+        System.out.println("Make sure stack is cleared. Otherwise throw a warning...");
+    }
+
+    private void publishMapToDB() {
+        System.out.println("Publishing the Tree to DB. After Checking that the data in the Tree is not null " + generalTree.size());
+    }
+
+    private Node createNode(String key, String value, String rulesetID){
+        if(key.equals("02") || key.equals("05") || key.equals("06") || key.equals("07"))
+        {
+            Node node = Node.builder()
+                    .type(key)
+                    .value(value)
+                    .leftID(indexCounter + 1)
+                    .rightID(indexCounter + 2)
+                    .rulesetID(rulesetID)
+                    .build();
+            indexCounter = indexCounter+2;
+            return node;
+        }
+        else {
+            Node node = Node.builder()
+                    .type(key)
+                    .value(value)
+                    .leftID(indexCounter + 1)
+                    .rulesetID(rulesetID)
+                    .build();
+            indexCounter++;
+            return node;
         }
     }
 }
