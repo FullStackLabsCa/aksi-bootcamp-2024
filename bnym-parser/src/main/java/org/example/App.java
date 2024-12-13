cebdd7835162b15b8ec2b5c5e009f9f6a192a96c
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-12-13 16:10:50 -0500

    BNYM Parser - Added Time Monitoring.

--- bnym-parser/src/main/java/org/example/App.java
+++ bnym-parser/src/main/java/org/example/App.java
@@ -1,10 +1,18 @@
 package org.example;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class App
 {
     public static void main( String[] args )
     {
+        LocalDateTime startTime = LocalDateTime.now();
         FileReader fileReader = new FileReader();
         fileReader.readFile("bony_sal_ruleset_d050514.data");
+
+        Duration duration = Duration.between(startTime, LocalDateTime.now());
+        System.out.println("duration = " + duration.getSeconds());
+
     }
 }
