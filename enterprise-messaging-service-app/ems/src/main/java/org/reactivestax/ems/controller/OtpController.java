d43888ee13d11fae37b574c71f14e886e6698c3c
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-20 13:33:36 -0500

    ems-app: OtpController - added annotations for rest Controller and autowired the otpService bean.

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/OtpController.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/OtpController.java
@@ -1,4 +1,15 @@
 package org.reactivestax.ems.controller;
 
+import org.reactivestax.ems.service.OtpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/otp")
 public class OtpController {
+
+    @Autowired
+    private OtpService otpService;
+
 }
