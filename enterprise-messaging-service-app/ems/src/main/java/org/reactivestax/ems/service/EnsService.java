a87d8b6954edf46c4c418f722cf62099607facf4
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-20 15:09:57 -0500

    ems-app: added logic to create customer, save the message to db and send the messageId to the jms for further processing.

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/service/EnsService.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/service/EnsService.java
@@ -1,4 +1,47 @@
 package org.reactivestax.ems.service;
 
+import org.reactivestax.ems.domain.Customer;
+import org.reactivestax.ems.domain.Message;
+import org.reactivestax.ems.dto.CustomerDTO;
+import org.reactivestax.ems.enums.DeliveryMode;
+import org.reactivestax.ems.enums.MessageType;
+import org.reactivestax.ems.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EnsService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    public void sendMessageViaSms(CustomerDTO customerDTO) {
+        createCustomerAndSendIdToJms(customerDTO, DeliveryMode.SMS, MessageType.Message);
+    }
+
+    private void createCustomerAndSendIdToJms(CustomerDTO customerDTO, DeliveryMode deliveryMode, MessageType messageType) {
+        Customer customer = customerRepository.findByCustomerId(customerDTO.getCustomerId());
+        Message message = Message.builder()
+                .deliveryMode(deliveryMode)
+                .messageType(messageType)
+                .messageData(customerDTO.getMessage())
+                .build();
+        customer.setMessage(message);
+
+        customerRepository.save(customer);
+
+        jmsTemplate.convertAndSend(message.getId());
+    }
+
+    public void sendMessageViaCall(CustomerDTO customerDTO) {
+        createCustomerAndSendIdToJms(customerDTO, DeliveryMode.CALL, MessageType.Message);
+    }
+
+    public void sendMessageViaEmail(CustomerDTO customerDTO) {
+        createCustomerAndSendIdToJms(customerDTO, DeliveryMode.EMAIL, MessageType.Message);
+    }
 }
