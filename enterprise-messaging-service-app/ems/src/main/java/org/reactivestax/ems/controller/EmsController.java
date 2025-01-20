1bfad68a7a085bc8706e71a84aba1d338a0cf3ec
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-20 13:32:46 -0500

    ems-app: EmsController - added annotations for rest Controller and autowired the emsService bean.

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/EmsController.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/EmsController.java
@@ -1,4 +1,17 @@
 package org.reactivestax.ems.controller;
 
+import org.reactivestax.ems.service.EmsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ems")
 public class EmsController {
+
+    @Autowired
+    private EmsService emsService;
+
+
+
 }
