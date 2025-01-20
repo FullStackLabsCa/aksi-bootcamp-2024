fd5e72ce64ee701ee3b8bbd7b4080f2a2310bb9a
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-20 15:11:54 -0500

    ems-app: removed unused imports

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/repository/CustomerRepository.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/repository/CustomerRepository.java
@@ -3,7 +3,6 @@ package org.reactivestax.ems.repository;
 import org.reactivestax.ems.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
     Customer findByCustomerId(String customerId);
