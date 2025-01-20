a377df998b1211af68a8261d9409e9e8f74093b1
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-20 14:36:29 -0500

    ems-app: Removed some fields not being used in CustomerDTO

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/dto/CustomerDTO.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/dto/CustomerDTO.java
@@ -1,22 +1,14 @@
 package org.reactivestax.ems.dto;
 
 import lombok.Data;
-import org.reactivestax.ems.MessageType;
-
-import java.time.LocalDateTime;
 
 @Data
 public class CustomerDTO {
     private Long id;
     private String customerId;
-    private Long phoneNumber;
-    private String emailAddress;
-
-    private MessageType messageType;
 
     private String message;
     private String otp;
 
     private Boolean verificationStatus;
-    private LocalDateTime updatedTime;
 }
