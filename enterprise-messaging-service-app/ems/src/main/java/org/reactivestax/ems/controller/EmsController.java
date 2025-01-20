b26b0450f053128af76c7b6563ceb5343f09469e
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-20 13:43:50 -0500

    ems-app: EmsController - added post message requests for sms, call, and email.

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/EmsController.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/EmsController.java
@@ -1,7 +1,12 @@
 package org.reactivestax.ems.controller;
 
+import org.reactivestax.ems.dto.CallDTO;
+import org.reactivestax.ems.dto.EmailDTO;
+import org.reactivestax.ems.dto.SmsDTO;
 import org.reactivestax.ems.service.EmsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +17,19 @@ public class EmsController {
     @Autowired
     private EmsService emsService;
 
+    @PostMapping("/sms")
+    public void sendMessageViaSms(@RequestBody SmsDTO smsDTO){
 
+    }
+
+    @PostMapping("/call")
+    public void sendMessageViaCall(@RequestBody CallDTO callDTO){
+
+    }
+
+    @PostMapping("/email")
+    public void sendMessageViaEmail(@RequestBody EmailDTO emailDTO){
+
+    }
 
 }
