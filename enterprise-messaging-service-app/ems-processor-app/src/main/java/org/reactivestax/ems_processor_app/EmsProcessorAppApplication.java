343c19caf4f076cd5925727d5890d1153c53c2c6
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-21 14:09:02 -0500

    ems-processor-app: project init

---
+++ enterprise-messaging-service-app/ems-processor-app/src/main/java/org/reactivestax/ems_processor_app/EmsProcessorAppApplication.java
@@ -0,0 +1,13 @@
+package org.reactivestax.ems_processor_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsProcessorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsProcessorAppApplication.class, args);
+	}
+
+}
---
+++ enterprise-messaging-service-app/ems-processor-app/src/test/java/org/reactivestax/ems_processor_app/EmsProcessorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reactivestax.ems_processor_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsProcessorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
