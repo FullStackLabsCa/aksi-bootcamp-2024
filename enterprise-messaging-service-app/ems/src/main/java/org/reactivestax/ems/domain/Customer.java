5e65f4fa72b01abf61f35e0035b22b9bc62800bb
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-22 16:16:42 -0500

    ems-app: added updateTimestamp annotation for updatedTime

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/domain/Customer.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/domain/Customer.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -28,6 +29,7 @@ public class Customer {
 
     @Builder.Default
     private LocalDateTime creationTime = LocalDateTime.now();
+    @UpdateTimestamp
     private LocalDateTime updatedTime;
 
 }
