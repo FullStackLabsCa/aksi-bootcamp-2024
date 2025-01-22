890892780106468649c3fcc53d780099adccf881
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-22 15:34:05 -0500

    ems-app: validation - introduced validation annotations for the CustomerDTO. and Created interfaces for groups

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/dto/CustomerDTO.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/dto/CustomerDTO.java
@@ -1,10 +1,23 @@
 package org.reactivestax.ems.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.reactivestax.ems.validation.MessageGroup;
+import org.reactivestax.ems.validation.OtpCreationGroup;
+import org.reactivestax.ems.validation.OtpVerifyGroup;
 
 @Data
 public class CustomerDTO {
     private Long id;
+
+    @NotNull
     private String customerId;
+
+    @NotNull(groups = {MessageGroup.class, OtpVerifyGroup.class})
+    @Null(groups = {OtpCreationGroup.class})
+    @Size(min = 6, max = 6, groups = {OtpVerifyGroup.class})
+    @Size(min = 1, groups = {MessageGroup.class})
     private String message;
 }
