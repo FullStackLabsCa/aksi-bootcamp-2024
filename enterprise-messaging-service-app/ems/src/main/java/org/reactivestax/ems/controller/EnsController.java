308a79297c3c829899c6d4f1f1ace3052b83a8a1
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-22 15:35:22 -0500

    ems-app: validation - added validation groups for the validations in EnsController

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/EnsController.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/EnsController.java
@@ -3,8 +3,10 @@ package org.reactivestax.ems.controller;
 import jakarta.validation.Valid;
 import org.reactivestax.ems.dto.CustomerDTO;
 import org.reactivestax.ems.service.EnsService;
+import org.reactivestax.ems.validation.MessageGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,19 +20,19 @@ public class EnsController {
     private EnsService ensService;
 
     @PostMapping("/sms")
-    public ResponseEntity<String> sendMessageViaSms(@Valid @RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<String> sendMessageViaSms(@RequestBody @Validated(MessageGroup.class) CustomerDTO customerDTO){
         ensService.sendMessageViaSms(customerDTO);
         return  ResponseEntity.ok("Message Sent Via SMS.");
     }
 
     @PostMapping("/call")
-    public ResponseEntity<String> sendMessageViaCall(@Valid @RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<String> sendMessageViaCall(@RequestBody @Validated(MessageGroup.class) CustomerDTO customerDTO){
         ensService.sendMessageViaCall(customerDTO);
         return  ResponseEntity.ok("Message Sent Via Call.");
     }
 
     @PostMapping("/email")
-    public ResponseEntity<String> sendMessageViaEmail(@Valid @RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<String> sendMessageViaEmail(@RequestBody @Validated(MessageGroup.class) CustomerDTO customerDTO){
         ensService.sendMessageViaEmail(customerDTO);
         return  ResponseEntity.ok("Message Sent Via Email.");
     }
