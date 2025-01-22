38e37d9b1edb7b0e7f37fa40ffac8e4877f261f0
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-22 15:47:31 -0500

    ems-app: validation - added validation groups for the validations in OtpController

--- enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/OtpController.java
+++ enterprise-messaging-service-app/ems/src/main/java/org/reactivestax/ems/controller/OtpController.java
@@ -2,9 +2,12 @@ package org.reactivestax.ems.controller;
 
 import org.reactivestax.ems.dto.CustomerDTO;
 import org.reactivestax.ems.service.OtpService;
+import org.reactivestax.ems.validation.OtpCreationGroup;
+import org.reactivestax.ems.validation.OtpVerifyGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,7 +27,7 @@ public class OtpController {
      * TODO
      */
     @PostMapping("/sms")
-    public ResponseEntity<String> sendOTPViaSms(@RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<String> sendOTPViaSms(@RequestBody @Validated(OtpCreationGroup.class) CustomerDTO customerDTO){
         boolean otpSendStatus = otpService.sendOtpViaSms(customerDTO);
         if(otpSendStatus) return  ResponseEntity.ok("OTP Sent Via SMS.");
         else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -32,7 +35,7 @@ public class OtpController {
     }
 
     @PostMapping("/call")
-    public ResponseEntity<String> sendOTPViaCall(@RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<String> sendOTPViaCall(@RequestBody @Validated(OtpCreationGroup.class) CustomerDTO customerDTO){
         boolean otpSendStatus = otpService.sendOtpViaCall(customerDTO);
         if(otpSendStatus) return  ResponseEntity.ok("OTP Sent Via Call.");
         else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -40,7 +43,7 @@ public class OtpController {
     }
 
     @PostMapping("/email")
-    public ResponseEntity<String> sendOTPViaEmail(@RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<String> sendOTPViaEmail(@RequestBody @Validated(OtpCreationGroup.class) CustomerDTO customerDTO){
         boolean otpSendStatus = otpService.sendOtpViaEmail(customerDTO);
         if(otpSendStatus) return  ResponseEntity.ok("OTP Sent Via Email.");
         else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -48,7 +51,7 @@ public class OtpController {
     }
 
     @PostMapping("/verify")
-    public ResponseEntity<String> verifyCustomerForOTP(@RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<String> verifyCustomerForOTP(@RequestBody @Validated(OtpVerifyGroup.class) CustomerDTO customerDTO){
         boolean isOtpValid = otpService.verifyOtp(customerDTO);
         if(isOtpValid) return ResponseEntity.ok("Customer Verified");
         else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
