de2cc34ab01cabb627538281a7fb139b1fbeb1db
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-24 12:17:15 -0500

    ems-app: testing - EnsController - sendMessageWithSms testing

---
+++ enterprise-messaging-service-app/ems/src/test/java/org/reactivestax/ems/controller/EnsControllerTest.java
@@ -0,0 +1,55 @@
+package org.reactivestax.ems.controller;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestax.ems.TestDataProvider;
+import org.reactivestax.ems.dto.CustomerDTO;
+import org.reactivestax.ems.service.EnsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(EnsController.class)
+public class EnsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private EnsService ensService;
+
+    @Test
+    void testSendMessageWithSmsGoodCustomer() throws  Exception {
+        /**
+         * send the CustomerJson (Good One)
+         * expect the method to execute successfully
+         * expect ResponseEntity.ok()
+         * expect the body of the ResponseEntity to be "Message Sent Via SMS."
+         */
+        String customerJson = TestDataProvider.goodCustomerJson.get();
+
+        doNothing().when(ensService).sendMessageViaSms(any(CustomerDTO.class));
+
+        mockMvc.perform(post("/api/ens/sms")
+                .content(customerJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Message Sent Via SMS."));
+    }
+
+}
