eb3603d74ba9db5c83e6630310f0ee2d34b81fc8
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-24 12:16:48 -0500

    ems-app: testing - added a Supplier Interface for testDataProvider for Ens.

---
+++ enterprise-messaging-service-app/ems/src/test/java/org/reactivestax/ems/TestDataProvider.java
@@ -0,0 +1,75 @@
+package org.reactivestax.ems;
+
+import java.util.function.Supplier;
+
+public interface TestDataProvider {
+
+    Supplier<String> goodCustomerJson = () -> """
+        {
+          "customerId": "testData",
+          "message": "This is my Message through SMS! Hello Test"
+        }
+        """;
+
+    Supplier<String> goodCustomerJsonWithPhoneNumber = () -> """
+        {
+          "customerId": "testData",
+          "message": "This is my Message through SMS! Hello Test",
+          "phoneNumber": "9055985456"
+        }
+        """;
+
+    Supplier<String> goodCustomerJsonWithEmail = () -> """
+        {
+          "customerId": "testData",
+          "message": "This is my Message through SMS! Hello Test",
+          "emailAddress": "Test@gmail.com"
+        }
+        """;
+
+    Supplier<String> goodCustomerJsonWithPhoneNumAndEmail = () -> """
+        {
+          "customerId": "testData",
+          "message": "This is my Message through SMS! Hello Test",
+          "phoneNumber": "9055985456",
+          "emailAddress": "Test@gmail.com"
+        }
+        """;
+
+    Supplier<String> badCustomerJsonWithBlankCustomerId = () -> """
+        {
+            "message": "This is a message with blank CustomerId
+        }
+        """;
+
+    Supplier<String> badCustomerJsonWithInvalidCustomerId = () -> """
+        {
+          "customerId": "doesNotExist",
+          "message": "This is my Message through SMS! Hello Test"
+        }
+        """;
+
+    Supplier<String> badCustomerJsonWithWrongPhoneNumLen = () -> """
+        {
+          "customerId": "testData",
+          "message": "This is my Message through SMS! Hello Test",
+          "phoneNumber": "90559854"
+        }
+        """;
+
+    Supplier<String> badCustomerJsonWithWrongPhoneNumData = () -> """
+        {
+          "customerId": "testData",
+          "message": "This is my Message through SMS! Hello Test",
+          "phoneNumber": "90559854aa"
+        }
+        """;
+
+    Supplier<String> badCustomerJsonWithWrongEmail = () -> """
+        {
+          "customerId": "testData",
+          "message": "This is my Message through SMS! Hello Test",
+          "emailAddress": "testData"
+        }
+        """;
+}
