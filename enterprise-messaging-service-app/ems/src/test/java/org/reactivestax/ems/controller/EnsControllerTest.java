929848d6ef29421a1fb6e870aae7ba205071113d
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-24 12:20:15 -0500

    ems-app: testing - EnsController - sendMessageWithSms good cases tested

--- enterprise-messaging-service-app/ems/src/test/java/org/reactivestax/ems/controller/EnsControllerTest.java
+++ enterprise-messaging-service-app/ems/src/test/java/org/reactivestax/ems/controller/EnsControllerTest.java
@@ -38,4 +38,43 @@ public class EnsControllerTest {
                 .andExpect(content().string("Message Sent Via SMS."));
     }
 
+    @Test
+    void testSendMessageWithSmsGoodCustomerWithPhoneNumber() throws  Exception {
+        String customerJson = TestDataProvider.goodCustomerJsonWithPhoneNumber.get();
+
+        doNothing().when(ensService).sendMessageViaSms(any(CustomerDTO.class));
+
+        mockMvc.perform(post("/api/ens/sms")
+                        .content(customerJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Message Sent Via SMS."));
+    }
+
+    @Test
+    void testSendMessageWithSmsGoodCustomerWithEmail() throws  Exception {
+        String customerJson = TestDataProvider.goodCustomerJsonWithEmail.get();
+
+        doNothing().when(ensService).sendMessageViaSms(any(CustomerDTO.class));
+
+        mockMvc.perform(post("/api/ens/sms")
+                        .content(customerJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Message Sent Via SMS."));
+    }
+
+    @Test
+    void testSendMessageWithSmsGoodCustomerWithPhNumAndEmail() throws  Exception {
+        String customerJson = TestDataProvider.goodCustomerJsonWithPhoneNumAndEmail.get();
+
+        doNothing().when(ensService).sendMessageViaSms(any(CustomerDTO.class));
+
+        mockMvc.perform(post("/api/ens/sms")
+                        .content(customerJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Message Sent Via SMS."));
+    }
+
 }
