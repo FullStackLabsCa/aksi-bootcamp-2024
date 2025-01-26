df5c02657a2106101043c5dfad625cd4a5eb77e4
Akam <aakamsingla@gmail.com>
2025-01-26 15:34:58 -0500

    ems-app: testing - otpService - added tests for sendOtpViaEmail

--- enterprise-messaging-service-app/ems/src/test/java/org/reactivestax/ems/service/OtpServiceTest.java
+++ enterprise-messaging-service-app/ems/src/test/java/org/reactivestax/ems/service/OtpServiceTest.java
@@ -191,4 +191,79 @@ class OtpServiceTest {
 //        verify(jmsTemplate, times(0)).convertAndSend("myDefaultQueue", Optional.ofNullable(any()));
     }
 
+    @Test
+    void testSendOtpViaEmail_ValidCustomer_ValidOtp(){
+        CustomerDTO customerDTO = TestDataProvider.goodCustomerDtoWithoutMessage.get();
+        Customer customer = TestDataProvider.goodCustomer.get();
+        Message message = TestDataProvider.otpMessage.get();
+
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(message);
+        Page<Message> mockPage = new PageImpl<>(messages, PageRequest.of(0, 1), 1);
+
+        doReturn(customer).when(customerRepository).findByCustomerId(any(String.class));
+        doReturn(mockPage).when(messageRepository).findByCustomer_CustomerIdAndMessageTypeAndCreationTimeAfter(any(String.class), any(MessageType.class), any(LocalDateTime.class), any(PageRequest.class));
+
+        assertTrue(otpService.sendOtpViaEmail(customerDTO));
+
+        verify(messageRepository, times(1)).save(any(Message.class));
+//        verify(jmsTemplate, times(1)).convertAndSend("myDefaultQueue", Optional.ofNullable(any()));
+    }
+
+    @Test
+    void testSendOtpViaEmail_ValidCustomer_OtpWithFailureCountReached(){
+        CustomerDTO customerDTO = TestDataProvider.goodCustomerDtoWithoutMessage.get();
+        Customer customer = TestDataProvider.goodCustomer.get();
+        Message message = TestDataProvider.otpMessageWithFailureCountReached.get();
+
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(message);
+        Page<Message> mockPage = new PageImpl<>(messages, PageRequest.of(0, 1), 1);
+
+        doReturn(customer).when(customerRepository).findByCustomerId(any(String.class));
+        doReturn(mockPage).when(messageRepository).findByCustomer_CustomerIdAndMessageTypeAndCreationTimeAfter(any(String.class), any(MessageType.class), any(LocalDateTime.class), any(PageRequest.class));
+
+        assertThrows(MaxOTPFailureCountReachedException.class, () -> otpService.sendOtpViaEmail(customerDTO));
+
+        verify(messageRepository, times(0)).save(any(Message.class));
+//        verify(jmsTemplate, times(0)).convertAndSend("myDefaultQueue", Optional.ofNullable(any()));
+    }
+
+    @Test
+    void testSendOtpViaEmail_ValidCustomer_OtpWithGenerationCountReached(){
+        CustomerDTO customerDTO = TestDataProvider.goodCustomerDtoWithoutMessage.get();
+        Customer customer = TestDataProvider.goodCustomer.get();
+        Message message = TestDataProvider.otpMessage.get();
+
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(message);
+        messages.add(message);
+        messages.add(message);
+        messages.add(message);
+        messages.add(message);
+        Page<Message> mockPage = new PageImpl<>(messages, PageRequest.of(0, 1), 1);
+
+        doReturn(customer).when(customerRepository).findByCustomerId(any(String.class));
+        doReturn(mockPage).when(messageRepository).findByCustomer_CustomerIdAndMessageTypeAndCreationTimeAfter(any(String.class), any(MessageType.class), any(LocalDateTime.class), any(PageRequest.class));
+
+        assertThrows(MaxOTPGenerationCountReachedException.class, () -> otpService.sendOtpViaEmail(customerDTO));
+
+        verify(messageRepository, times(0)).save(any(Message.class));
+//        verify(jmsTemplate, times(0)).convertAndSend("myDefaultQueue", Optional.ofNullable(any()));
+    }
+
+    @Test
+    void testSendOtpViaEmail_InvalidCustomer(){
+        CustomerDTO customerDTO = TestDataProvider.goodCustomerDtoWithoutMessage.get();
+        Customer customer = TestDataProvider.goodCustomer.get();
+
+        doReturn(null).when(customerRepository).findByCustomerId(any(String.class));
+
+        assertThrows(CustomerNotFoundException.class, () -> otpService.sendOtpViaEmail(customerDTO));
+
+        verify(messageRepository, times(0)).save(any(Message.class));
+        verify(messageRepository, times(0)).findByCustomer_CustomerIdAndMessageTypeAndCreationTimeAfter(any(String.class), any(MessageType.class), any(LocalDateTime.class), any(PageRequest.class));
+//        verify(jmsTemplate, times(0)).convertAndSend("myDefaultQueue", Optional.ofNullable(any()));
+    }
+
 }
