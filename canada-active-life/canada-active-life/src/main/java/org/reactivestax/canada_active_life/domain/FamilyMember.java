9a025eb3983f0c91ba25cec7ed9c381e452856c4
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-28 15:15:35 -0500

    Canada-active-life: added entity annotations for FamilyMember table

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/FamilyMember.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/FamilyMember.java
@@ -1,4 +1,34 @@
 package org.reactivestax.canada_active_life.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class FamilyMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int familyMemberId;
+
+    private String name;
+    private String dob;
+    private String gender;
+
+    private String emailAddress;
+    private String streetNumber;
+    private String streetName;
+    private String city;
+    private String province;
+    private String country;
+    private String homePhoneNumber;
+    private String businessPhoneNumber;
+
+    private String language;
+    private String memberLoginId;
+    private boolean isActive;
+
+    @ManyToOne
+    @JoinColumn(name = "familyGroupId")
+    private FamilyGroup familyGroup;
 }
