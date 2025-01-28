54f6e5f1bf9b34c11fb9f809ba270087a49f92c4
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-28 14:54:34 -0500

    Canada-active-life: added RequestMapping paths for Controllers

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/OfferedCourseController.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/OfferedCourseController.java
@@ -1,4 +1,9 @@
 package org.reactivestax.canada_active_life.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cal/offered_course")
 public class OfferedCourseController {
 }
--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/RegistrationController.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/RegistrationController.java
@@ -1,4 +1,8 @@
 package org.reactivestax.canada_active_life.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cal/registration")
 public class RegistrationController {
 }
