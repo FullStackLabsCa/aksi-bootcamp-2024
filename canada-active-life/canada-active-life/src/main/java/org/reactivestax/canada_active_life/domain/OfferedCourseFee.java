91e138fc037d0f907fe8dab7b1d89ead1bb54d7f
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-28 15:30:42 -0500

    Canada-active-life: added entity annotations for OfferedCourseFee table

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/OfferedCourseFee.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/OfferedCourseFee.java
@@ -1,4 +1,33 @@
 package org.reactivestax.canada_active_life.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class OfferedCourseFee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int feeId;
+
+    private String feeType;
+
+    private double courseFee;
+
+    @ManyToOne
+    @JoinColumn(name = "offeredCourseId")
+    private OfferedCourse offeredCourseId;
+
+    @CreationTimestamp
+    private LocalDateTime createdTimeStamp;
+    @UpdateTimestamp
+    private LocalDateTime updatedTimeStamp;
+
+    private String createdBy;
+    private String updatedBy;
 }
