a27c7b92b79d2d462521d7899b15b08dd693f3fa
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-28 15:55:04 -0500

    Canada-active-life: added entity annotations for FamilyCourseRegistration table

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/FamilyCourseRegistration.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/FamilyCourseRegistration.java
@@ -1,4 +1,43 @@
 package org.reactivestax.canada_active_life.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class FamilyCourseRegistration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int familyCourseRegistrationId;
+
+    private double cost;
+    private LocalDate enrollmentDate;
+
+    private boolean isWithdrawn;
+    private double withdrawnCredits;
+
+    private String enrollmentActor;
+    private int enrollmentActorId;
+
+    @ManyToOne
+    @JoinColumn(name = "offeredCourseId")
+    private OfferedCourse offeredCourseId;
+
+    @ManyToOne
+    @JoinColumn(name = "familyMemberId")
+    private FamilyMember familyMemberId;
+
+    @CreationTimestamp
+    private LocalDateTime createdTimeStamp;
+    @UpdateTimestamp
+    private LocalDateTime updatedTimeStamp;
+
+    private String createdBy;
+    private String updatedBy;
 }
