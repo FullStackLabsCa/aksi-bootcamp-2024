2df26d52256ac922c012e386d6ec70b9274f3cd5
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-28 14:13:41 -0500

    Canada-active-life: Extracted and created an intelliJ Project

---
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/CanadaActiveLifeApplication.java
@@ -0,0 +1,13 @@
+package org.reactivestax.canada_active_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanadaActiveLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanadaActiveLifeApplication.class, args);
+	}
+
+}
---
+++ canada-active-life/canada-active-life/src/test/java/org/reactivestax/canada_active_life/CanadaActiveLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reactivestax.canada_active_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanadaActiveLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
