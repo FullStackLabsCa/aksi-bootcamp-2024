285e552ea1aa908b5c0025d97ca37a363793bbd3
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-28 15:06:28 -0500

    Canada-active-life: added entityAnnotations for FamilyGroup table

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/FamilyGroup.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/FamilyGroup.java
@@ -1,4 +1,36 @@
 package org.reactivestax.canada_active_life.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class FamilyGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int familyGroupId;
+
+    private String familyPin;
+
+    private double credits;
+
+    private String status;
+
+    private long failedLoginAttempts;
+
+    @CreationTimestamp
+    private LocalDateTime createdTimeStamp;
+
+    private String createdBy;
+
+    private LocalDateTime updatedTimeStamp;
+
+    private String updateBy;
 }
