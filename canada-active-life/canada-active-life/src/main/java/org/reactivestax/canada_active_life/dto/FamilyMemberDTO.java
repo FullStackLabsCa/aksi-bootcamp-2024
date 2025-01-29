bdfa8337088dfcaffe6fb7634f394e60958e0825
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-29 16:09:59 -0500

    Canada-active-life: added fields to FamilyMemberDTO

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/FamilyMemberDTO.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/FamilyMemberDTO.java
@@ -1,4 +1,39 @@
 package org.reactivestax.canada_active_life.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class FamilyMemberDTO {
+
+    private int id;
+
+    private String name;
+
+    private LocalDate dob;
+
+    private String gender;
+
+    private String emailAddress;
+
+    private String streetNumber;
+
+    private String streetName;
+
+    private String city;
+
+    private String province;
+
+    private String country;
+
+    private String homePhoneNumber;
+
+    private String businessPhoneNumber;
+
+    private String preferredContactMethod;
+
+    private String language;
+
+    private String memberLoginId;
 }
