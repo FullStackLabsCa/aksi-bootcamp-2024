d215f69fed88b3038390f2cc01d2ee0e762fdb2a
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-29 15:55:45 -0500

    Canada-active-life: added fields to SearchCriteriaDTO

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/SearchCriteriaDTO.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/SearchCriteriaDTO.java
@@ -1,4 +1,23 @@
 package org.reactivestax.canada_active_life.dto;
 
+import lombok.Data;
+import org.reactivestax.canada_active_life.domain.AgeGroup;
+import org.reactivestax.canada_active_life.domain.Category;
+import org.reactivestax.canada_active_life.domain.Facility;
+import org.reactivestax.canada_active_life.domain.SubCategory;
+
+import java.time.LocalDate;
+
+@Data
 public class SearchCriteriaDTO {
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private String city;
+    private Facility facility;
+
+    private AgeGroup ageGroup;
+    private Category category;
+    private SubCategory subCategory;
 }
