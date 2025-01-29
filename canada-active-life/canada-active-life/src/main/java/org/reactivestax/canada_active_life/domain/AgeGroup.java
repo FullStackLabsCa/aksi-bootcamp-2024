681d8ecf9eb1fb0a4593e5629aaf03c380e6ca67
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-29 12:00:12 -0500

    Canada-active-life: Added fields for the Reference Table entity AgeGroup

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/AgeGroup.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/AgeGroup.java
@@ -5,6 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -13,4 +17,15 @@ public class AgeGroup {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int ageGroupId;
+
+    private String shortCode;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdTimeStamp;
+    @UpdateTimestamp
+    private LocalDateTime updatedTimeStamp;
+
+    private int createdBy;
+    private int updatedBy;
 }
