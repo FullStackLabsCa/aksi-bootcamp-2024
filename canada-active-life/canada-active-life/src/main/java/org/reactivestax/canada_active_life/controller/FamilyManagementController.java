7f5757db3b9d53eee0ce13a918ce0ef32489bbfb
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-29 16:35:47 -0500

    Canada-active-life: added method signatures for the family management controller

---
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/FamilyManagementController.java
@@ -0,0 +1,49 @@
+package org.reactivestax.canada_active_life.controller;
+
+import org.reactivestax.canada_active_life.dto.FamilyMemberDTO;
+import org.reactivestax.canada_active_life.service.FamilyManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/CanadaActiveLife/v1/members")
+public class FamilyManagementController {
+
+    @Autowired
+    private FamilyManagementService familyManagementService;
+
+    @PostMapping
+    public ResponseEntity<String> addFamilyMemberToExistingGroup(@RequestBody FamilyMemberDTO familyMemberDTO, @RequestHeader int actorId){
+        /**
+         *
+         */
+        return ResponseEntity.ok("Family Member added to group.");
+    }
+
+    @GetMapping
+    public ResponseEntity<FamilyMemberDTO> getFamilyMemberDetails(@RequestParam int familyMemberId){
+        /**
+         *
+         */
+        FamilyMemberDTO familyMemberDTO = null;
+        return ResponseEntity.ok(familyMemberDTO);
+    }
+
+    @PatchMapping
+    public ResponseEntity<String> updateFamilyMember(@RequestBody FamilyMemberDTO familyMemberDTO){
+        /**
+         *
+         */
+        return ResponseEntity.ok("Family Member info updated.");
+    }
+
+    @GetMapping
+    public ResponseEntity<String> deactivateFamilyMember(@RequestParam int familyMemberId, @RequestHeader int actorId){
+        /**
+         *
+         */
+        return ResponseEntity.ok("Family Member Deactivated");
+    }
+
+}
--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/FamilyMemberController.java
+++
@@ -1,9 +0,0 @@
-package org.reactivestax.canada_active_life.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/cal/family_member")
-public class FamilyMemberController {
-}
