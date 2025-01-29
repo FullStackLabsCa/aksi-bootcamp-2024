42378b0213d27cd93dfca951c50d7574c6bd2efd
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-29 16:14:22 -0500

    Canada-active-life: added fields to CourseMemberWaitlistDTO

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/CourseMemberWaitlistDTO.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/CourseMemberWaitlistDTO.java
@@ -1,4 +1,18 @@
 package org.reactivestax.canada_active_life.dto;
 
+import lombok.Data;
+import org.reactivestax.canada_active_life.domain.FamilyMember;
+import org.reactivestax.canada_active_life.domain.OfferedCourse;
+
+@Data
 public class CourseMemberWaitlistDTO {
+    private int id;
+
+    private String enrollmentActor;
+
+    private int enrollmentActorId;
+
+    private OfferedCourse offeredCourse;
+
+    private FamilyMember familyMember;
 }
