ffd62f489a17792f83c4f7c304baf9d202311031
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-29 15:52:13 -0500

    Canada-active-life: added fields to UserVerificationDTO

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/UserVerificationDTO.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/UserVerificationDTO.java
@@ -1,4 +1,11 @@
 package org.reactivestax.canada_active_life.dto;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class UserVerificationDTO {
+    private UUID uuidToken;
+    private String otpEnteredByUser;
 }
