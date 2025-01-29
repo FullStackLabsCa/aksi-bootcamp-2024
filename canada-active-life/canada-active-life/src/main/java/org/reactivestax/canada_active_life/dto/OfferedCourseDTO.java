e8130aca591d17c329632f0980824aee6b9781b3
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-29 16:03:54 -0500

    Canada-active-life: added fields to OfferedCourseDTO

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/OfferedCourseDTO.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/OfferedCourseDTO.java
@@ -1,4 +1,28 @@
 package org.reactivestax.canada_active_life.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
 public class OfferedCourseDTO {
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private int numOfClassesOffered;
+
+    private int seatsAvailable;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private boolean isAllDayCourse;
+
+    private LocalDate registrationStartDate;
+
+    private String availableForEnrollment;
 }
