a2b3d62f64fd9a35997d86352074b7d22d50bf14
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-29 16:17:00 -0500

    Canada-active-life: added fields to CourseMemberRegistrationDTO

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/CourseMemberRegistrationDTO.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/dto/CourseMemberRegistrationDTO.java
@@ -1,4 +1,29 @@
 package org.reactivestax.canada_active_life.dto;
 
+import lombok.Data;
+import org.reactivestax.canada_active_life.domain.FamilyMember;
+import org.reactivestax.canada_active_life.domain.OfferedCourse;
+
+import java.time.LocalDate;
+
+@Data
 public class CourseMemberRegistrationDTO {
+
+    private int id;
+
+    private double cost;
+
+    private LocalDate enrollmentDate;
+
+    private boolean isWithdrawn;
+
+    private double withdrawnCredits;
+
+    private String enrollmentActor;
+
+    private int enrollmentActorId;
+
+    private OfferedCourse offeredCourseId;
+
+    private FamilyMember familyMemberId;
 }
