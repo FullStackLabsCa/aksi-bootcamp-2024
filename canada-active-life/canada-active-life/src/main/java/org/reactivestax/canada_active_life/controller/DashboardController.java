a678598fb071de08d7b8c2a88590cb01c849e857
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-30 09:53:36 -0500

    Canada-active-life: DashboardController - added service call to familyManagementService for signup member and activate account

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/DashboardController.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/DashboardController.java
@@ -1,9 +1,39 @@
 package org.reactivestax.canada_active_life.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestax.canada_active_life.dto.FamilyMemberDTO;
+import org.reactivestax.canada_active_life.service.FamilyManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/cal/dashboard")
+@RequestMapping("/CanadaActiveLife/v1")
 public class DashboardController {
+
+    @Autowired
+    private FamilyManagementService familyManagementService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUpNewMember(@RequestBody FamilyMemberDTO familyMemberDTO){
+
+        boolean isMemberCreated = familyManagementService.signUpNewFamilyMember(familyMemberDTO);
+
+        if(isMemberCreated) return ResponseEntity.ok("Family Member created. Please click on the Activation Link in the email to activate the account.");
+        else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Family Member could not be created.");
+    }
+
+    @GetMapping("/activate-account")
+    public ResponseEntity<String> activateNewMember(@PathVariable String uuid, @PathVariable String memberLoginId){
+
+        boolean isMemberActivated = familyManagementService.activateNewSignUp(Integer.parseInt(memberLoginId), UUID.fromString(uuid));
+
+        if(isMemberActivated) return ResponseEntity.ok("Family Member activated.");
+        else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Family Member could not be activated.");
+    }
+
 }
