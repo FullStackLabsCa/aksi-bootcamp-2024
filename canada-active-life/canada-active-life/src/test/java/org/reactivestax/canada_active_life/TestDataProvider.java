92e7e335d8fa2ae721c261f72b1857fc036b3d8e
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-30 12:06:37 -0500

    CanadaActiveLife: FamilyManagementService - SignUp - RestAssured Test - Updated Test Data Provider

--- canada-active-life/canada-active-life/src/test/java/org/reactivestax/canada_active_life/TestDataProvider.java
+++ canada-active-life/canada-active-life/src/test/java/org/reactivestax/canada_active_life/TestDataProvider.java
@@ -2,10 +2,26 @@ package org.reactivestax.canada_active_life;
 
 import org.reactivestax.canada_active_life.dto.FamilyMemberDTO;
 
+import java.time.LocalDate;
 import java.util.function.Supplier;
 
 public interface TestDataProvider {
     Supplier<FamilyMemberDTO> goodFamilyMemberDTO = () ->
             FamilyMemberDTO.builder()
+                    .name("Jane Doe")  // Setting string value for 'name'
+                    .dob(LocalDate.parse("1992-08-15"))  // Parsing string to LocalDate for 'dob'
+                    .gender("Female")
+                    .emailAddress("jane.doe@example.com")
+                    .streetNumber("456")
+                    .streetName("Elm Street")
+                    .city("Springfield")
+                    .province("Illinois")
+                    .country("USA")
+                    .homePhoneNumber("555-123-4567")
+                    .businessPhoneNumber("555-987-6543")
+                    .preferredContactMethod("Email")
+                    .language("English")
+                    .memberLoginId("janedoe99")
+                    .familyPin("FAM123456")
                     .build();
 }
