61d92fbb203e23f0573d54bfd5663ef4b71e7964
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-31 10:58:48 -0500

    Canada-active-life: FamilyManagement - Completed

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/OfferedCourseController.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/OfferedCourseController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cal/offered_course")
+@RequestMapping("/CanadaActiveLife/v1/offeredCourses")
 public class OfferedCourseController {
 }
