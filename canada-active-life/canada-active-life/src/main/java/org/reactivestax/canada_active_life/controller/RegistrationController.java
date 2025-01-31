712f7bdd5a0628949652942a4dce52ab301b86c5
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-31 15:16:42 -0500

    CanadaActiveLife: CourseRegistration - Enrollment - Set Up Controller EP's for create and get enrollments

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/RegistrationController.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/RegistrationController.java
@@ -1,8 +1,34 @@
 package org.reactivestax.canada_active_life.controller;
 
+import org.reactivestax.canada_active_life.dto.CourseMemberRegistrationDTO;
+import org.reactivestax.canada_active_life.service.OfferedCourseRegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/cal/registration")
+@RequestMapping("/CanadaActiveLife/v1/courseRegistrations")
 public class RegistrationController {
+
+    @Autowired
+    private OfferedCourseRegistrationService offeredCourseRegistrationService;
+
+    @PostMapping("/enrollment")
+    public ResponseEntity<String> enrollFamilyMemberInAnOfferedCourse(@RequestBody CourseMemberRegistrationDTO courseMemberRegistrationDTO){
+        boolean isEnrolled = offeredCourseRegistrationService.enrollFamilyMemberInOfferedCourse(courseMemberRegistrationDTO);
+        if(isEnrolled) return ResponseEntity.ok("Family Member Enrolled in the Offered Course.");
+        else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Failed to enroll the Family Member in the desired offered course...");
+    }
+
+    @GetMapping("/enrollment")
+    public ResponseEntity<List<CourseMemberRegistrationDTO>> getAllCourseEnrollmentsForMember(@RequestParam String familyMemberId){
+        List<CourseMemberRegistrationDTO> enrollments = offeredCourseRegistrationService.getEnrollmentsForMember(Integer.parseInt(familyMemberId));
+        if(!enrollments.isEmpty()) return ResponseEntity.ok(enrollments);
+        else return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
 }
