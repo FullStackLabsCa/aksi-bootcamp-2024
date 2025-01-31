ea6c33977d24b20450855ac2829ff0be60fa23f9
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-01-31 10:47:57 -0500

    CanadaActiveLife: FamilyManagementService - Added 2FA Verification Logic

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/PendingLoginUUID.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/PendingLoginUUID.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class PendingLoginUUID {
 
     @Id
