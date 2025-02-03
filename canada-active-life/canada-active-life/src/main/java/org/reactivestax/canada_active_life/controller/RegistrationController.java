61b9dd8c8a37627d656797d71364138b6e065dda
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-02-03 10:57:37 -0500

    CanadaActiveLife: Get Mapping for Enrollment and Waitlist - Added Controller EPs.

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/RegistrationController.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package org.reactivestax.canada_active_life.controller;
 
 import org.reactivestax.canada_active_life.dto.CourseMemberRegistrationDTO;
+import org.reactivestax.canada_active_life.dto.CourseMemberWaitlistDTO;
 import org.reactivestax.canada_active_life.service.RegistrationManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,8 +26,8 @@ public class RegistrationController {
     }
 
     @GetMapping("/enrollment")
-    public ResponseEntity<List<CourseMemberRegistrationDTO>> getAllCourseEnrollmentsForMember(@RequestParam String familyMemberId){
-        List<CourseMemberRegistrationDTO> enrollments = registrationManagementService.getEnrollmentsForMember(Integer.parseInt(familyMemberId));
+    public ResponseEntity<List<CourseMemberRegistrationDTO>> getAllCourseEnrollmentsForMember(@RequestParam String memberLoginId){
+        List<CourseMemberRegistrationDTO> enrollments = registrationManagementService.getEnrollmentsForMember(memberLoginId);
         if(!enrollments.isEmpty()) return ResponseEntity.ok(enrollments);
         else return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
@@ -37,6 +38,13 @@ public class RegistrationController {
         if(isWithdrawn) return ResponseEntity.ok("Family Member withdrawn from the Offered Course.");
         else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body("Failed to withdraw the Family Member from the offered course...");
+    }
+
+    @GetMapping("/waitlist")
+    public ResponseEntity<List<CourseMemberWaitlistDTO>> getAllCourseWaitlistForMember(@RequestParam String memberLoginId){
+        List<CourseMemberWaitlistDTO> waitlist = registrationManagementService.getWaitlistForMember(memberLoginId);
+        if(!waitlist.isEmpty()) return ResponseEntity.ok(waitlist);
+        else return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
 }
