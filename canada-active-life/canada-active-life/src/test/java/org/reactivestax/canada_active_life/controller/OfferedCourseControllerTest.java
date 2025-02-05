f058efb97d93e8b4a2bb403e2bf157540fbdb5a0
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-02-05 18:02:46 -0500

    CanadaActiveLife: Testing - Controller - OfferedCourse - testCreateNewOfferedCourse - Done - Without Validations

--- canada-active-life/canada-active-life/src/test/java/org/reactivestax/canada_active_life/controller/OfferedCourseControllerTest.java
+++ canada-active-life/canada-active-life/src/test/java/org/reactivestax/canada_active_life/controller/OfferedCourseControllerTest.java
@@ -1,10 +1,23 @@
 package org.reactivestax.canada_active_life.controller;
 
+import org.junit.jupiter.api.Test;
+import org.reactivestax.canada_active_life.TestDataProvider.FamilyMemberCourseRegistrationTestDataProvider;
+import org.reactivestax.canada_active_life.dto.FamilyMemberCourseRegistrationDTO;
+import org.reactivestax.canada_active_life.dto.OfferedCourseDTO;
 import org.reactivestax.canada_active_life.service.OfferedCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(OfferedCourseController.class)
 class OfferedCourseControllerTest {
@@ -14,5 +27,37 @@ class OfferedCourseControllerTest {
 
     @MockitoBean
     private OfferedCourseService offeredCourseService;
+
+    @Test
+    void testCreateNewOfferedCourse_Successful() throws Exception {
+        String uriTemplate = "/CanadaActiveLife/v1/offeredCourses";
+        String expectedOutcome = "Offered Course created.";
+        String familyMemberCourseRegistrationDTO = FamilyMemberCourseRegistrationTestDataProvider.validRegistrationDTOJson.get();
+
+        when(offeredCourseService.createNewOfferedCourse(any(OfferedCourseDTO.class)))
+                .thenReturn(true);
+
+        mockMvc.perform(post(uriTemplate)
+                        .content(familyMemberCourseRegistrationDTO)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedOutcome));
+    }
+
+    @Test
+    void testCreateNewOfferedCourse_Unsuccessful() throws Exception {
+        String uriTemplate = "/CanadaActiveLife/v1/offeredCourses";
+        String expectedOutcome = "Offered Course could not be created.";
+        String familyMemberCourseRegistrationDTO = FamilyMemberCourseRegistrationTestDataProvider.validRegistrationDTOJson.get();
+
+        when(offeredCourseService.createNewOfferedCourse(any(OfferedCourseDTO.class)))
+                .thenReturn(false);
+
+        mockMvc.perform(post(uriTemplate)
+                        .content(familyMemberCourseRegistrationDTO)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(expectedOutcome));
+    }
 
 }
