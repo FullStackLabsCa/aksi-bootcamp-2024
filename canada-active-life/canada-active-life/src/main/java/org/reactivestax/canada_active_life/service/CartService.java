d88a5c12030d50b825251d9e6b4b2ef9616bdfd7
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-02-06 12:54:21 -0500

    CanadaActiveLife: Cart Feature - added pseudo code in service for add, get, remove, checkout, and pay

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/service/CartService.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/service/CartService.java
@@ -1,6 +1,7 @@
 package org.reactivestax.canada_active_life.service;
 
 import org.reactivestax.canada_active_life.dto.CartDTO;
+import org.reactivestax.canada_active_life.dto.PaymentDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,10 +12,10 @@ public class CartService {
     public boolean addOfferedCourseToCart(CartDTO cartDTO, String actorLoginId) {
         /**
          * Check actor validity
+         * check if offeredCourse is "open"
          * Check if there is seat available in the offered Course
          *      Yes - add to cart
          *      No - throw Exception
-         * add to cart in the db
          */
         return false;
     }
@@ -23,7 +24,6 @@ public class CartService {
         /**
          * check actor validity
          * query the table to get all the cart for the actor ID
-         *
          */
         return null;
     }
@@ -37,6 +37,28 @@ public class CartService {
     }
 
     public boolean checkoutCart(String actorLoginId){
+        /**
+         * get cart for actor
+         * for each item in cart
+         *      call first half of the enroll in registration service
+         *          make the change to add in the table for unconfirmedPaymentRegistrations
+         */
+        return false;
+    }
+
+    public boolean payForCart(PaymentDTO paymentDTO, String actorMemberLoginId){
+        /**
+         * validate actor
+         * validate there are existing items in the unconfirmedPaymentRegistrations - because of expiration time
+         *      if no - throw exception paymentSessionExpired - try again
+         * for the totalAmount in the cart - prepare and send stripe a payment confirmedIntent
+         * if stripe success
+         *      add to enrollments - second half of the enroll member
+         *      remove from the unconfirmedPaymentRegistrations
+         * if stripe fails
+         *      keep in unconfirmedPaymentRegistrations
+         *      exit with exception saying payment failed
+         */
         return false;
     }
 }
