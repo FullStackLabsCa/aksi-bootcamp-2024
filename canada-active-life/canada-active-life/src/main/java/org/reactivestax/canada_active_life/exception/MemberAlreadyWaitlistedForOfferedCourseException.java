b1dc8e8447496edf8fd74ce999c0706d47ff1b6d
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-02-10 16:37:19 -0500

    CanadaActiveLife: Testing - RegistrationController Test - 100 % completed

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/exception/MemberAlreadyWaitlistedForOfferedCourseException.java
+++
@@ -1,7 +0,0 @@
-package org.reactivestax.canada_active_life.exception;
-
-public class MemberAlreadyWaitlistedForOfferedCourseException extends RuntimeException {
-    public MemberAlreadyWaitlistedForOfferedCourseException(String message) {
-        super(message);
-    }
-}
