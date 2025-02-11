b809746e38ef75c687044deed17a9b060f7da303
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-02-11 10:44:26 -0500

    CanadaActiveLife: Testing - CartManagementService - Add to Cart - Good Case - Covered

--- canada-active-life/canada-active-life/src/test/java/org/reactivestax/canada_active_life/service/CartManagementServiceTest.java
+++ canada-active-life/canada-active-life/src/test/java/org/reactivestax/canada_active_life/service/CartManagementServiceTest.java
@@ -1,15 +1,40 @@
 package org.reactivestax.canada_active_life.service;
 
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import org.reactivestax.canada_active_life.TestDataProvider.CartManagementTestDataProvider;
+import org.reactivestax.canada_active_life.domain.Cart;
+import org.reactivestax.canada_active_life.domain.FamilyMember;
+import org.reactivestax.canada_active_life.domain.OfferedCourse;
 import org.reactivestax.canada_active_life.dto.CartDTO;
+import org.reactivestax.canada_active_life.mapper.CartMapper;
+import org.reactivestax.canada_active_life.repo.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-public class CartManagementServiceTest {
+@SpringBootTest
+class CartManagementServiceTest {
+
+    @Autowired
+    private CartManagementService cartManagementService;
+
+    @MockitoBean
+    private FamilyManagementService familyManagementService;
+
+    @MockitoBean
+    private OfferedCourseService offeredCourseService;
+
+    @MockitoBean
+    private RegistrationManagementService registrationManagementService;
+
+    @MockitoBean
+    private CartRepository cartRepository;
+
+    @Autowired
+    private CartMapper cartMapper;
 
     @Test
     void testAddToCart_ActorDNE(){}
@@ -30,7 +55,39 @@ public class CartManagementServiceTest {
     void testAddToCart_NoSeatsAvailableInOfferedCourse(){}
 
     @Test
-    void testAddToCart_SeatsAvailable(){}
+    void testAddToCart_SeatsAvailable(){
+        FamilyMember familyMember = FamilyMember.builder().build();
+        OfferedCourse offeredCourse = OfferedCourse.builder().build();
+        Cart cart = Cart.builder()
+                .cartId(1)
+                .offeredCourse(OfferedCourse.builder().offeredCourseId(1).build())
+                .familyMember(FamilyMember.builder().memberLoginId("anyFamilyMember").build())
+                .build();
+
+        when(familyManagementService.checkFamilyMemberValidity(any(String.class)))
+                .thenReturn(familyMember);
+        when(offeredCourseService.getOfferedCourseById(any(Integer.class)))
+                .thenReturn(offeredCourse);
+        when(registrationManagementService.checkFamilyMemberAndOfferedCourseValidity(any(FamilyMember.class), any(OfferedCourse.class)))
+                .thenReturn(true);
+        when(registrationManagementService.getCostOfOfferedCourseForFamilyMember(any(OfferedCourse.class), any(FamilyMember.class)))
+                .thenReturn(100.0);
+        when(cartRepository.save(any(Cart.class)))
+                .thenReturn(cart);
+
+        CartDTO cartDTO = CartDTO.builder()
+                .familyMemberLoginId("anyFamilyMember")
+                .offeredCourseId(1)
+                .build();
+        CartDTO cartDTOResponse = cartManagementService.addOfferedCourseToCart(cartDTO, "anyActor");
+
+        assertNotNull(cartDTOResponse);
+        assertNotNull(cartDTOResponse.getId());
+        verify(familyManagementService, times(2)).checkFamilyMemberValidity(any(String.class));
+        verify(registrationManagementService, times(1)).checkFamilyMemberAndOfferedCourseValidity(any(FamilyMember.class), any(OfferedCourse.class));
+        verify(registrationManagementService, times(1)).getCostOfOfferedCourseForFamilyMember(any(OfferedCourse.class), any(FamilyMember.class));
+        verify(cartRepository, times(1)).save(any(Cart.class));
+    }
 
     @Test
     void testGetCart_ActorDNE(){}
