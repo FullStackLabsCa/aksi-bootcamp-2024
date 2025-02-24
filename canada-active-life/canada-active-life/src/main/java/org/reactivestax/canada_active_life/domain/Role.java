9417b812835123697e7903e34a09ce2ef0cf3fda
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-02-24 11:20:34 -0500

    CanadaActiveLife-security - added Security Constants

---
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/domain/Role.java
@@ -0,0 +1,12 @@
+package org.reactivestax.canada_active_life.domain;
+
+import org.reactivestax.canada_active_life.enums.ROLE;
+
+public class Role {
+
+    private int roleId;
+
+    private String memberLoginId;
+
+    private ROLE role;
+}
---
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/enums/ROLE.java
@@ -0,0 +1,8 @@
+package org.reactivestax.canada_active_life.enums;
+
+public enum ROLE {
+    ADMIN,
+    FAMILY_MEMBER,
+    FAMILY_GROUP_OWNER,
+    UNVERIFIED
+}
