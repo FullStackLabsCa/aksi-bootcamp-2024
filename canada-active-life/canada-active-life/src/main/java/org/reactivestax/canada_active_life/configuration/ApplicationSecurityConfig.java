2d3132cf6304a067b22c1832de3b1c10f018d11a
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-02-24 12:32:32 -0500

    CanadaActiveLife-security - added SpringSecurityConfig

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/configuration/ApplicationSecurityConfig.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/configuration/ApplicationSecurityConfig.java
@@ -1,7 +1,9 @@
 package org.reactivestax.canada_active_life.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.reactivestax.canada_active_life.web.security.JwtAuthenticationFilter;
 import org.reactivestax.canada_active_life.web.security.JwtAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,24 +17,27 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
             .csrf(csrfConfigurer -> csrfConfigurer.disable())  // Disabling CSRF protection
-            .cors(corsConfigurer -> corsConfigurer.disable())  // Disabling CORS (not recommended)
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()  // Allow Swagger UI access
                     .requestMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll() // Allow API Docs access
                     .requestMatchers(HttpMethod.POST, "/CanadaActiveLife/v1/signup").permitAll()
                     .requestMatchers(HttpMethod.GET, "/CanadaActiveLife/v1/activate-account").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/CanadaActiveLife/v1/browse_offered_courses").permitAll()
-                    .anyRequest().authenticated()) // Require authentication for other endpoints
-                .addFilter(new JwtAuthenticationFilter(authenticationManager))
+                    .requestMatchers(HttpMethod.POST, "/CanadaActiveLife/v1/browse_offered_courses").permitAll())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())// Require authentication for other endpoints
+                .addFilterBefore(new JwtAuthenticationFilter(authenticationManager, objectMapper), UsernamePasswordAuthenticationFilter.class)
                 .addFilter(new JwtAuthorizationFilter(authenticationManager))
 //            .formLogin(Customizer.withDefaults()) // Enable Form Login
             .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
