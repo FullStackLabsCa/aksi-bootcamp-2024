89dddacef5bb582e408cbb89b98afe02232976ca
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-02-24 11:21:05 -0500

    CanadaActiveLife-security - updated UserDetailsService to provide the user from repo

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/web/security/AppUserDetailsService.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/web/security/AppUserDetailsService.java
@@ -1,17 +1,37 @@
 package org.reactivestax.canada_active_life.web.security;
 
+import org.reactivestax.canada_active_life.domain.FamilyMember;
+import org.reactivestax.canada_active_life.service.FamilyManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Configuration
 public class AppUserDetailsService implements UserDetailsService {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private FamilyManagementService familyManagementService;
+
+    public AppUserDetailsService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String memberLoginId) throws UsernameNotFoundException {
+        FamilyMember familyMember = familyManagementService.checkFamilyMemberValidity(memberLoginId);
+
+        return User.withUsername(familyMember.getMemberLoginId())
+                .password(familyMember.getFamilyGroup().getFamilyPin())
+                .authorities("UNVERIFIED")
+                .passwordEncoder(passwordEncoder::encode)
+                .build();
     }
 }
