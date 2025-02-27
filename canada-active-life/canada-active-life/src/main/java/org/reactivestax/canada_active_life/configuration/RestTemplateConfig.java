815f559b401812667670062527177c6a57dd867c
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-02-27 13:55:39 -0500

    CanadaActiveLife-security - 2way ssl with ems - removed unused imports

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/configuration/RestTemplateConfig.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/configuration/RestTemplateConfig.java
@@ -4,18 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() throws Exception {
-        return RestTemplateFactory.createRestTemplate("src/main/resources/keystore.p12", "src/main/resources/truststore.p12", "changeit");
+        return RestTemplateFactory.createRestTemplate("src/main/resources/cal-keystore.p12", "src/main/resources/cal-truststore.p12", "changeit");
     }
 }
