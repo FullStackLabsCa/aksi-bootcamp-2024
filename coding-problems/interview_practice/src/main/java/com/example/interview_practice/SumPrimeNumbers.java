3a2da24a467a42dc221137b5eb23c947c4ffa6f8
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-03-17 13:24:32 -0400

    Interview Coding Problems - SumOfPrimeNumbers

---
+++ coding-problems/interview_practice/src/main/java/com/example/interview_practice/SumPrimeNumbers.java
@@ -0,0 +1,26 @@
+package com.example.interview_practice;
+
+import java.util.stream.IntStream;
+
+public class SumPrimeNumbers {
+
+    private static boolean isNumberPrime(int number){
+        int factors = 0;
+
+        for(int i = 1; i <= number; i++){
+            if(number%i == 0)
+                factors++;
+        }
+
+        if(factors == 2) return true;
+        return false;
+    }
+
+    private static int sumOfPrimeNumbers(int n){
+        return IntStream.rangeClosed(1, n).filter(p -> isNumberPrime(p)).sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfPrimeNumbers(1000));
+    }
+}
