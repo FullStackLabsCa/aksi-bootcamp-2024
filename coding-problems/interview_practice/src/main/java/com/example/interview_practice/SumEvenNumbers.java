8f1661d0e857f18bd1c4dab87b1ae9361f101ed2
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-03-17 13:08:21 -0400

    Interview Coding Problems - SumOfEvenNumbers

--- coding-problems/interview_practice/src/main/java/com/example/interview_practice/SumEvenNumbers.java
+++ coding-problems/interview_practice/src/main/java/com/example/interview_practice/SumEvenNumbers.java
@@ -1,4 +1,24 @@
 package com.example.interview_practice;
 
+import java.util.stream.IntStream;
+
 public class SumEvenNumbers {
+
+    private static int calculateSumOfEvenNumbers(int n){
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            if(i % 2  == 0)
+                sum = sum + i;
+        }
+        return sum;
+    }
+
+    private static int calculateSumOfEvenUsingStreams(int n){
+        return IntStream.rangeClosed(1, n).filter(i -> i % 2 == 0).sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateSumOfEvenNumbers(50));
+        System.out.println(calculateSumOfEvenUsingStreams(50));
+    }
 }
