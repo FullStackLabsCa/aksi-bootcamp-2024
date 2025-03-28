1a1a90ed0cab13fda9067e01af5ce8d18a521151
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-03-28 13:46:40 -0400

    Interview Prep - Coding - Find Word With Specific Num Of Vowels

--- coding-problems/interview_practice/src/main/java/com/example/stream_questions/FindWordWithSpecificVowels.java
+++ coding-problems/interview_practice/src/main/java/com/example/stream_questions/FindWordWithSpecificVowels.java
@@ -1,7 +1,6 @@
 package com.example.stream_questions;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FindWordWithSpecificVowels {
@@ -17,7 +16,6 @@ public class FindWordWithSpecificVowels {
                     return (size == numOfVowels);
                 })
                 .forEach(System.out::println);
-
     }
 
     public static void main(String[] args) {
