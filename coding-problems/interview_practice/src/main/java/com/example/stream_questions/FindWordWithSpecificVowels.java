42ec7c7fd6741ea342d16c4e27e9af7f1e45c904
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-03-28 13:45:51 -0400

    Interview Prep - Coding - Find Word With Specific Num Of Vowels

---
+++ coding-problems/interview_practice/src/main/java/com/example/stream_questions/FindWordWithSpecificVowels.java
@@ -0,0 +1,26 @@
+package com.example.stream_questions;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FindWordWithSpecificVowels {
+
+    private static void findWordWithSpecificNumOfVowels(String testString, int numOfVowels){
+        String[] words = testString.trim().split(" ");
+
+        Arrays.stream(words)
+                .filter((word) -> {
+                    long size = IntStream.range(0, word.length()).mapToObj(index -> word.toCharArray()[index])
+                            .filter((character -> "aeiou".contains(character.toString().toLowerCase())))
+                            .count();
+                    return (size == numOfVowels);
+                })
+                .forEach(System.out::println);
+
+    }
+
+    public static void main(String[] args) {
+        findWordWithSpecificNumOfVowels("I am learning Streams API in Java", 2);
+    }
+}
