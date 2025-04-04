a07a633882d19ba40bb66c53cd0edf5c4f8bb938
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-04 11:42:11 -0400

    Canada-Active-Life-React : Better UI for Welcome Page

--- canada-active-life-react/src/components/Welcome.jsx
+++ canada-active-life-react/src/components/Welcome.jsx
@@ -1,16 +1,18 @@
-import {Link} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
+import Courses from "./Courses.jsx";
+import {Button} from "react-bootstrap";
 
 function Welcome() {
     return (
         <>
-            <h1>Welcome to Canada Active Life</h1>
-            <p>Where all get active....</p>
-            <nav>
-                <Link to="/aboutUs">Learn More About Us Here</Link>
-            </nav>
-            {/*<Routes>*/}
-            {/*    <Route path="/aboutUs" element={<AboutUs />} />*/}
-            {/*</Routes>*/}
+            <div className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+                <h1>Welcome to Canada Active Life</h1>
+                <p>Where all get active....</p>
+                <Button as={Link} to="/browseCourses" className="mt-3">Browse Courses</Button>
+            </div>
+            <Routes>
+                <Route path="/browseCourses" element={<Courses />} />
+            </Routes>
         </>
     )
 }
