c7fc5f8b75f031b20dcd231dbc06f4cb5c65afbb
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-04 11:10:03 -0400

    Canada-Active-Life-React : Added UI Component for Course and updated the fields

--- canada-active-life-react/src/components/Course.jsx
+++ canada-active-life-react/src/components/Course.jsx
@@ -1,9 +1,29 @@
-function Course() {
-    return (
-        <>
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import {Button} from "react-bootstrap";
 
-        </>
-    )
+function Course({course}) {
+    return (
+        <div className="m-3">
+            <Card style={{width: '18rem'}}>
+                <Card.Body>
+                    <Card.Title>{course.course.name}</Card.Title>
+                    <Card.Text>
+                        {course.course.description}
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>{course.facility.name}</ListGroup.Item>
+                    <ListGroup.Item>{course.facility.city}, {course.facility.province}</ListGroup.Item>
+                    <ListGroup.Item>Enrollment: {course.availableForEnrollment}</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Button className="d-block mb-1">Add To Cart</Button>
+                    <Button className="d-block">Enroll for Course</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 }
 
-export default Course
+export default Course;
