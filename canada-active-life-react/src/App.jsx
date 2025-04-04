a81ec2e7f22c380941e56239c33dc37a2f6fe324
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-04 11:33:43 -0400

    Canada-Active-Life-React : Introduced NavigationBar Using BootStrap

--- canada-active-life-react/src/App.jsx
+++ canada-active-life-react/src/App.jsx
@@ -3,26 +3,13 @@ import './App.css'
 import Welcome from "./components/Welcome.jsx";
 import Courses from "./components/Courses.jsx";
 import Login from "./components/Login.jsx";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NavigationBar from "./components/NavigationBar.jsx";
 
 function App() {
     return (
         <BrowserRouter>
-            <nav className="nav-bar">
-                <Link to="/">Welcome</Link>
-                <span> | </span>
-                <Link to="/browseCourses">Courses</Link>
-                <span> | </span>
-                <Link to="/login">Login</Link>
-                <span> | </span>
-                <Link to="/signup">SignUp</Link>
-                <span className="not-display"> | </span>
-                <Link to="/cart" className="not-display">Cart</Link>
-                <span className="not-display"> | </span>
-                <Link to="/checkout" className="not-display">Checkout</Link>
-                <span className="not-display"> | </span>
-                <Link to="/dashboard" className="not-display">Dashboard</Link>
-            </nav>
+            <NavigationBar />
             <Routes>
                 <Route path="/" element={<Welcome/>}/>
                 <Route path="/browseCourses" element={<Courses/>}/>
--- canada-active-life-react/src/components/NavigationBar.jsx
+++ canada-active-life-react/src/components/NavigationBar.jsx
@@ -1,7 +1,33 @@
-export default function NavigationBar() {
-    return (
-        <>
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Link} from "react-router-dom";
 
-        </>
-    )
+function NavigationBar() {
+    return (
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">Canada Active Life</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+
+                    </Nav>
+                    <Nav>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/browseCourses">Courses</Nav.Link>
+                        <NavDropdown title="Account" id="collapsible-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="/login">Login</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={Link} to="/signup">Sign Up</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link as={Link} to="/aboutUs">About Us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
+
+export default NavigationBar;
