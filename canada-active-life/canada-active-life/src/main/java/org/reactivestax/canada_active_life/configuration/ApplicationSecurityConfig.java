5e726ae2eb374e02371b2cbfee94656f243cf04a
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-04 09:56:44 -0400

    Canada-Active-Life-React : Added Cors configuration to accept requests from the frontend application

--- canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/configuration/ApplicationSecurityConfig.java
+++ canada-active-life/canada-active-life/src/main/java/org/reactivestax/canada_active_life/configuration/ApplicationSecurityConfig.java
@@ -2,6 +2,7 @@ package org.reactivestax.canada_active_life.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.filters.CorsFilter;
 import org.reactivestax.canada_active_life.service.FamilyManagementService;
 import org.reactivestax.canada_active_life.web.security.JwtAuthenticationFilter;
 import org.reactivestax.canada_active_life.web.security.JwtAuthorizationFilter;
@@ -19,6 +20,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -32,12 +38,13 @@ public class ApplicationSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
             .csrf(csrfConfigurer -> csrfConfigurer.disable())  // Disabling CSRF protection
-            .authorizeHttpRequests(auth -> auth
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(auth -> auth
                     .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()  // Allow Swagger UI access
                     .requestMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll() // Allow API Docs access
                     .requestMatchers(HttpMethod.POST, "/CanadaActiveLife/v1/signup").permitAll()
                     .requestMatchers(HttpMethod.GET, "/CanadaActiveLife/v1/activate-account").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/CanadaActiveLife/v1/browse_offered_courses").permitAll())
+                    .requestMatchers(  "/CanadaActiveLife/v1/browse_offered_courses").permitAll())
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())// Require authentication for other endpoints
                 .addFilterBefore(new JwtAuthenticationFilter(authenticationManager, objectMapper, familyManagementService), UsernamePasswordAuthenticationFilter.class)
                 .addFilter(new JwtAuthorizationFilter(authenticationManager))
@@ -45,6 +52,20 @@ public class ApplicationSecurityConfig {
             .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:5173"); // Allow frontend URL
+        corsConfiguration.addAllowedMethod("*"); // Allow all HTTP methods
+        corsConfiguration.addAllowedHeader("*"); // Allow all headers
+        corsConfiguration.setAllowCredentials(true); // Allow credentials
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 
     @Bean
