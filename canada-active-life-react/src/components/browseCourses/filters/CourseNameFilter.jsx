3e85ae7f3cfb3f7f427a40da82fda4782f51f30c
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-08 10:14:01 -0400

    Canada-Active-Life-React : Login -> Fixing a minor warning in CourseNameFilter to handle undefined filteredOfferedCoursesState.FilterWord

--- canada-active-life-react/src/components/browseCourses/filters/CourseNameFilter.jsx
+++ canada-active-life-react/src/components/browseCourses/filters/CourseNameFilter.jsx
@@ -4,7 +4,7 @@ export default function CourseNameFilter(props) {
     return <input className="form-control mb-3"
                   type="text"
                   placeholder="Course Name"
-                  value={props.filteredOfferedCoursesState.filterWord}
+                  value={props.filteredOfferedCoursesState.filterWord ?? ""}
                   onChange={props.onChange
                   }/>;
 }
