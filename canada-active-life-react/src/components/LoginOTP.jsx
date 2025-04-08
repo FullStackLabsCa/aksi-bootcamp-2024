95c4dc25f02a4f28cabb111bc99c1c6d10fccfa4
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-08 11:11:26 -0400

    Canada-Active-Life-React : Login -> Created a OTP Pop Up Window that shows up on Successful userId Password

---
+++ canada-active-life-react/src/components/LoginOTP.jsx
@@ -0,0 +1,33 @@
+import { Modal, Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
+
+function LoginOTP({ show, onHide, onVerify }) {
+    const [otp, setOtp] = useState('');
+
+    const handleSubmit = () => {
+        onVerify(otp);
+    };
+
+    return (
+        <Modal show={show} onHide={onHide} centered>
+            <Modal.Header closeButton>
+                <Modal.Title>Enter 6-digit OTP</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter OTP"
+                    value={otp}
+                    maxLength={6}
+                    onChange={(e) => setOtp(e.target.value)}
+                />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>Cancel</Button>
+                <Button variant="primary" onClick={handleSubmit}>Submit</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default LoginOTP;
