bcc0997976cc10621d2a849bb7687970cddc01d2
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-08 11:11:02 -0400

    Canada-Active-Life-React : Login -> Created LoginError Toast on wrong UserId, Password or Wrong OTP

---
+++ canada-active-life-react/src/components/LoginError.jsx
@@ -0,0 +1,30 @@
+import Toast from 'react-bootstrap/Toast';
+import {useEffect} from "react";
+import ToastContext from "react-bootstrap/ToastContext";
+import {ToastContainer} from "react-bootstrap";
+
+function LoginError({show, message, onClose}) {
+
+    useEffect(() => {
+        if (show) {
+            const timer = setTimeout(() => {
+                onClose();
+            }, 3000); // 2 seconds
+
+            return () => clearTimeout(timer);
+        }
+    }, [show, onClose]);
+
+    return (
+        <ToastContainer position='top-center' className='p-3'>
+            <Toast show={show} onClose={onClose} bg='danger'>
+                <Toast.Header closeButton>
+                    <strong className="me-auto">Login Failed</strong>
+                </Toast.Header>
+                <Toast.Body className='text-white'>{message}</Toast.Body>
+            </Toast>
+        </ToastContainer>
+    );
+}
+
+export default LoginError;
