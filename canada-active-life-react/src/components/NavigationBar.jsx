3556d292954544371e32b604ed27c9473f04bbe9
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-09 10:56:21 -0400

    Canada-Active-Life-React : LogOut -> Added Logout Button to navigation bar on login and functionality working

--- canada-active-life-react/src/components/NavigationBar.jsx
+++ canada-active-life-react/src/components/NavigationBar.jsx
@@ -3,9 +3,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Link} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {updateLoginStatus, updateMemberLoginId} from "../store/slices/memberSlice.jsx";
 
 function NavigationBar() {
+
+    const dispatch = useDispatch()
+
+    const handleLogOut = () => {
+        dispatch(updateLoginStatus({loginStatus: false}))
+        dispatch(updateMemberLoginId({memberLoginId: ''}))
+    }
+
     return (
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
             <Container>
@@ -20,14 +29,14 @@ function NavigationBar() {
                         <Nav.Link as={Link} to="/browseCourses">Courses</Nav.Link>
                         <NavDropdown title="Account" id="collapsible-nav-dropdown">
                             <NavDropdown.Item
-                                className={useSelector(state => state.member.isLoggedIn) ? 'visually-hidden' : 'show'}
+                                className={useSelector(state => state.member.isLoggedIn) ? 'visually-hidden' : 'showing'}
                                 as={Link}
                                 to="/login"
                             >
                                 Login
                             </NavDropdown.Item>
                             <NavDropdown.Item
-                                className={useSelector(state => state.member.isLoggedIn) ? 'show' : 'visually-hidden'}
+                                className={useSelector(state => state.member.isLoggedIn) ? 'showing' : 'visually-hidden'}
                                 as={Link}
                                 to="/dashboard"
                             >
@@ -35,18 +44,18 @@ function NavigationBar() {
                             </NavDropdown.Item>
 
                             <NavDropdown.Divider
-                                className={useSelector(state => state.member.isLoggedIn && !state.member.isAdmin) ? 'visually-hidden' : 'show'}
+                                className={useSelector(state => state.member.isLoggedIn && !state.member.isAdmin) ? 'visually-hidden' : 'showing'}
                             />
 
                             <NavDropdown.Item
-                                className={useSelector(state => state.member.isLoggedIn) ? 'visually-hidden' : 'show'}
+                                className={useSelector(state => state.member.isLoggedIn) ? 'visually-hidden' : 'showing'}
                                 as={Link}
                                 to="/signup"
                             >
                                 Sign Up
                             </NavDropdown.Item>
                             <NavDropdown.Item
-                                className={useSelector(state => state.member.isLoggedIn && state.member.isAdmin) ? 'show' : 'visually-hidden'}
+                                className={useSelector(state => state.member.isLoggedIn && state.member.isAdmin) ? 'showing' : 'visually-hidden'}
                                 as={Link}
                                 to="/addFamilyMemberToGroup"
                             >
@@ -54,6 +63,14 @@ function NavigationBar() {
                             </NavDropdown.Item>
                         </NavDropdown>
                         <Nav.Link as={Link} to="/aboutUs">About Us</Nav.Link>
+                        <Nav.Link
+                            className={useSelector(state => state.member.isLoggedIn) ? 'showing' : 'visually-hidden'}
+                            as={Link}
+                            to="/"
+                            onClick={handleLogOut}
+                        >
+                            Logout
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
