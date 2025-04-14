440dc2415836866cfa39ba8c1deb63782d63b0a6
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-14 16:07:59 -0400

    Canada-Active-Life-React : Dashboard - Saving LoginStatus in SessionStorage.

--- canada-active-life-react/src/store/slices/memberSlice.jsx
+++ canada-active-life-react/src/store/slices/memberSlice.jsx
@@ -3,12 +3,13 @@ import {createSlice} from "@reduxjs/toolkit";
 const memberSlice = createSlice({
     name: 'member',
     initialState: {
-        isLoggedIn: false,
+        isLoggedIn: sessionStorage.getItem('isLoggedIn'),
         memberLoginId: '',
         isAdmin: true
     },
     reducers: {
         updateLoginStatus: (state, action) => {
+            sessionStorage.setItem('isLoggedIn', action.payload.loginStatus)
             return {
                 ...state,
                 isLoggedIn: action.payload.loginStatus
