d716241044cdb706310b6a95539ca97bf670fb87
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2025-04-17 15:04:05 -0400

    Trade-Processor-App-Kafka-Native : Consumer - Configured KafkaConsumer

---
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/utility/messaging/kafka/KafkaUtils.java
@@ -0,0 +1,32 @@
+package io.reactivestax.utility.messaging.kafka;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaUtils {
+
+    private static Consumer<String, String> configureConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        return new KafkaConsumer<>(props);
+    }
+
+    public static Consumer<String, String> getConsumer() {
+        return configureConsumer();
+    }
+
+    public static void subscribeConsumerToTopic(Consumer<String, String> kafkaConsumer, String topicName) {
+        kafkaConsumer.subscribe(Collections.singleton(topicName));
+    }
+}
